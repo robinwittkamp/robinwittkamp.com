36b3809c0fd1f3023953fcb69bd0dce7f1b61445
Robin Wittkamp <robinwittkamp94@gmail.com>
2021-03-12 16:02:53 +0100

    PrivacyPolicy, Imprint: add placeholder content

---
+++ pages/imprint.js
@@ -0,0 +1,11 @@
+import Head from '../components/Head';
+import Layout from '../components/Layout';
+
+const Imprint = () => (
+  <Layout>
+    <Head />
+    <h1>Imprint</h1>
+  </Layout>
+);
+
+export default Imprint;
---
+++ pages/privacy-policy.js
@@ -0,0 +1,11 @@
+import Head from '../components/Head';
+import Layout from '../components/Layout';
+
+const PrivacyPolicy = () => (
+  <Layout>
+    <Head />
+    <h1>PrivacyPolicy</h1>
+  </Layout>
+);
+
+export default PrivacyPolicy;
