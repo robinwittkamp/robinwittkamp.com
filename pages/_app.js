84a341406277fe4d3337915b352bb085edeed772
Robin Wittkamp <robinwittkamp94@gmail.com>
2021-03-26 21:37:08 +0100

    fix eslint errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
