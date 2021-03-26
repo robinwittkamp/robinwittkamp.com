e64d4314b5f5bdfb51da0461dc780a1af2760c2c
Robin Wittkamp <robinwittkamp94@gmail.com>
2021-03-26 22:32:47 +0100

    add custom 'next-document'

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html
+        lang="en"
+        className="antialiased bg-white text-gray-900 dark:bg-gray-900  dark:text-white"
+      >
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
