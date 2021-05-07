ecdfdd88c20b73e1ae47e45435446c1c2ab2fa1e
Robin Wittkamp <robinwittkamp94@gmail.com>
2021-05-07 15:46:14 +0200

    Update 500.js

--- pages/500.js
+++ pages/500.js
@@ -7,7 +7,6 @@ const Custom500 = () => (
     <Head title="Server-side error occurred | Robin Wittkamp" />
     <span>{'<(☉︵☉)>'}</span>
     <h1>Server-side error occurred</h1>
-    {/* <p>The link you followed may be broken, or the page may have been removed.</p> */}
     <Link href="/">Home</Link>
   </Layout>
 );
