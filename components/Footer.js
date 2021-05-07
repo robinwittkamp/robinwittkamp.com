8406cf7d60b1887f93d9b86693a8dce90b99e43a
Robin Wittkamp <robinwittkamp94@gmail.com>
2021-05-07 20:27:16 +0200

    footer: add links

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,20 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 
 const Footer = () => (
   <footer>
-    <span>Footer</span>
+    <span className="text-gray-500 dark:text-true-gray-500">© 2021 robin wittkamp</span>
+    <ul>
+      <li>
+        <Link href="/imprint">
+          <a>Imprint</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/privacy-policy">
+          <a>Privacy Policy</a>
+        </Link>
+      </li>
+    </ul>
   </footer>
 );
 
