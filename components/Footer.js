2b1c2d8962c6f8da3ddc36d7eb6c91c2c91680b1
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-02-03 20:41:02 +0100

    Footer: improve styling, add data objects

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,37 @@
 import Link from 'next/link';
 
+const languages = [{ name: 'Deutsch' }, { name: 'English' }];
+
+const navigation = [
+  { name: 'Imprint', href: '/imprint' },
+  { name: 'Privacy policy', href: '/privacy-policy' },
+];
+
 const Footer = () => (
-  <footer>
-    <span className="text-gray-500 dark:text-true-gray-500">© 2021 robin wittkamp</span>
-    <ul>
-      <li>
-        <Link href="/imprint">
-          <a>Imprint</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/privacy-policy">
-          <a>Privacy Policy</a>
-        </Link>
-      </li>
+  <footer className="mt-16 space-y-3 pb-8">
+    <ul className="flex justify-center">
+      {languages.map((item) => (
+        <li key={item.name}>
+          <Link href="#">
+            <a className="block py-2 px-2 text-gray-600 dark:text-true-gray-300">{item.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
+
+    <ul className="flex justify-center">
+      {navigation.map((item) => (
+        <li key={item.name}>
+          <Link href={item.href}>
+            <a className="block py-2 px-2 text-gray-600 dark:text-true-gray-300">{item.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+
+    <span className="block py-2 px-2 text-center text-gray-500 dark:text-true-gray-400">
+      © 2022 robin wittkamp
+    </span>
   </footer>
 );
 
