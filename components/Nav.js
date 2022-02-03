fa8007a6b29c02ca5c30730ef4ae5bbf4e42c588
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-02-03 21:55:25 +0100

    Nav: improve Menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Menu } from '@headlessui/react';
 
 const navigation = [
   { name: 'Home', href: '#home' },
@@ -8,19 +9,66 @@ const navigation = [
 ];
 
 const Nav = () => (
-  <nav className="">
-    <a className="" />
+  <Menu as="div" className="h-full">
+    <Menu.Button className="h-full w-12">X</Menu.Button>
 
-    <ul className="hidden md:flex">
+    <Menu.Items as="ul" className="absolute left-0 right-0 w-full bg-white dark:bg-true-gray-900">
       {navigation.map((item) => (
-        <li key={item.name}>
+        <Menu.Item as="li" key={item.name}>
           <Link href={item.href}>
-            <a className="block h-full px-6 text-xl font-extrabold">{item.name}</a>
+            <a className="block px-4 py-3 text-lg font-extrabold lg:h-full lg:px-6 lg:text-xl">
+              {item.name}
+            </a>
           </Link>
-        </li>
+        </Menu.Item>
       ))}
-    </ul>
-  </nav>
+
+      {/* <Menu.Item as="li">
+        {({ active }) => (
+          <a className={`${active && 'bg-blue-500'}`} href="#home">
+            Home
+          </a>
+        )}
+      </Menu.Item>
+
+      <Menu.Item as="li">
+        {({ active }) => (
+          <a className={`${active && 'bg-blue-500'}`} href="#about">
+            About
+          </a>
+        )}
+      </Menu.Item>
+
+      <Menu.Item as="li">
+        {({ active }) => (
+          <a className={`${active && 'bg-blue-500'}`} href="#work">
+            Work
+          </a>
+        )}
+      </Menu.Item>
+
+      <Menu.Item as="li">
+        {({ active }) => (
+          <a className={`${active && 'bg-blue-500'}`} href="#contact">
+            Contact
+          </a>
+        )}
+      </Menu.Item> */}
+    </Menu.Items>
+  </Menu>
+  // <nav className="">
+  //   <a className="" />
+
+  //   <ul className="hidden md:flex">
+  //     {navigation.map((item) => (
+  //       <li key={item.name}>
+  //         <Link href={item.href}>
+  //           <a className="block h-full px-6 text-xl font-extrabold">{item.name}</a>
+  //         </Link>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // </nav>
 );
 
 export default Nav;
