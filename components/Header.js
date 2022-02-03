cfac3db9d6ca33a524d1afeed9102fe8a3374f7b
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-02-03 20:41:32 +0100

    Header: add link to logo

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,18 @@
+import Link from 'next/link';
 import Nav from './Nav';
 
 const Header = () => (
   <header className="sticky flex h-12 items-center justify-between md:h-20 lg:h-24 xl:h-24">
-    <span className="text-xl font-extrabold leading-[0.8] md:text-2xl md:leading-[0.8]">
-      robin
-      <br />
-      wittkamp
-    </span>
+    <Link href="/">
+      <a className="text-xl font-extrabold leading-[0.8] leading-[0.8] md:text-2xl md:leading-[0.8]">
+        <span className="">
+          robin
+          <br />
+          wittkamp
+        </span>
+      </a>
+    </Link>
+
     <Nav />
   </header>
 );
