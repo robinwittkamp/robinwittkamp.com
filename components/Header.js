2e2aeb1c79fb6821d00e0b270a9dbab38a4201ea
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-02-05 17:08:34 +0100

    Header: add mobile menu

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,64 @@
 import Link from 'next/link';
+import { Menu, Popover } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Nav from './Nav';
 
-const Header = () => (
-  <header className="firefox:bg-opacity-90 border-neutral-800 dark:bg-neutral-900/70 fixed top-0 h-14 w-full border-b bg-white/30 px-4 backdrop-blur sm:px-8 md:h-20 md:px-12 lg:h-24 lg:px-24 xl:h-24 xl:px-32">
-    <div className="flex h-full items-center justify-between">
-      <Link href="/">
-        <a className="text-xl font-black leading-[0.85] md:text-2xl md:leading-[0.85]">
-          <span className="">
-            robin
-            <br />
-            wittkamp
-          </span>
-        </a>
-      </Link>
+const navigation = [
+  { name: 'Home', href: '#home' },
+  { name: 'About', href: '#about' },
+  { name: 'Work', href: '#work' },
+  { name: 'Contact', href: '#contact' },
+];
 
-      <Nav />
-    </div>
+const Header = () => (
+  <header className="firefox:bg-opacity-90 fixed top-0 w-full border-b border-neutral-800 bg-white/30 backdrop-blur dark:bg-neutral-900/70">
+    {/* Popover */}
+    <Popover className="md:h-20 lg:h-24 xl:h-24">
+      {/* Padding container */}
+      <div className="px-4 sm:px-8 md:px-12 lg:px-24 xl:px-32">
+        {/* Logo & Navigation container */}
+        <div className="flex items-center justify-between">
+          {/* Logo container */}
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            {/* Logo */}
+            <Link href="/">
+              <a
+                href="#"
+                className="text-xl font-black leading-[0.85] md:text-2xl md:leading-[0.85]"
+              >
+                <span className="sr-only">Robin Wittkamp</span>
+                <span className="">
+                  robin
+                  <br />
+                  wittkamp
+                </span>
+              </a>
+            </Link>
+          </div>
+
+          {/* Mobil menu button */}
+          <div className="-mr-3 md:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-8 w-8" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobil navigation */}
+      <Popover.Panel className="">
+        <nav className="">
+          {navigation.map((item) => (
+            <Link key={item.name} href={item.href}>
+              <a className="block p-3">
+                <span className="text-lg font-extrabold ">{item.name}</span>
+              </a>
+            </Link>
+          ))}
+        </nav>
+      </Popover.Panel>
+    </Popover>
   </header>
 );
 
