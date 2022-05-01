af94b4e6ba3079854ea0de759775846ca38a7daa
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-01 13:57:51 +0200

    Improve logo text

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -2,13 +2,9 @@ import Link from 'next/link';
 
 const Logo = () => (
   <Link href="/">
-    <a href="#" className="text-xl font-black leading-[0.85] md:text-2xl md:leading-[0.85]">
+    <a className="text-xl font-bold md:text-2xl">
       <span className="sr-only">Robin Wittkamp</span>
-      <span className="">
-        robin
-        <br />
-        wittkamp
-      </span>
+      <span className="">Robin Wittkamp</span>
     </a>
   </Link>
 );
