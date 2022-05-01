053d9c8a2dd30382d9ae8804632b3871d5c576bb
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-01 15:10:13 +0200

    tailwind-config: add 'Gilroy' as custom utility class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Gilroy', ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      gilroy: ['Gilroy', ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
