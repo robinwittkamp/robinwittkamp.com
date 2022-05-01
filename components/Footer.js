20c04ac471d9ea987dbe99b5e70f4508ac66211f
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-01 15:38:05 +0200

    Footer: small improvements

--- components/Footer.js
+++ components/Footer.js
@@ -9,28 +9,31 @@ const navigation = [
 
 const Footer = () => (
   <footer className="mt-16 space-y-3 pb-8">
+    {/* Languages */}
     <ul className="flex justify-center">
       {languages.map((item) => (
         <li key={item.name}>
           <Link href="#">
-            <a className="dark:text-neutral-300 block py-2 px-2 text-gray-600">{item.name}</a>
+            <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">{item.name}</a>
           </Link>
         </li>
       ))}
     </ul>
 
+    {/* Links */}
     <ul className="flex justify-center">
       {navigation.map((item) => (
         <li key={item.name}>
           <Link href={item.href}>
-            <a className="dark:text-neutral-300 block py-2 px-2 text-gray-600">{item.name}</a>
+            <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">{item.name}</a>
           </Link>
         </li>
       ))}
     </ul>
 
-    <span className="dark:text-neutral-400 block py-2 px-2 text-center text-gray-500">
-      © 2022 robin wittkamp
+    {/* Copyright notice */}
+    <span className="block py-2 px-2 text-center text-gray-500 dark:text-neutral-400">
+      &copy; 2022 Robin Wittkamp
     </span>
   </footer>
 );
