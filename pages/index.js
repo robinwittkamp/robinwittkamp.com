b3cb9fa21f11181bf511b5407a90b347b5425083
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-01 18:04:44 +0200

    index: readd Gilroy font

--- pages/index.js
+++ pages/index.js
@@ -7,31 +7,33 @@ const Home = () => (
     <Head />
 
     <section id="home" className="px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h1 className="text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
+      <h1 className="font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
         Hi, I&apos;m Robin.
       </h1>
       <p className="mt-6 text-xl">
         I build fast, reliable websites with a simple, modern design and focus on user experience.
         The technologies I currently use the most are Next.js, React.js and WordPress.
       </p>
-      <a className="mt-6 block text-xl font-bold">View projects</a>
+      <a className="mt-6 block text-lg font-extrabold">View projects</a>
       {/* <HeroCanvas /> */}
     </section>
 
     <section id="about" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="text-center text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
+      <h2 className="text-center font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
         About me
       </h2>
     </section>
 
     <section id="work" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="text-center text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
+      <h2 className="text-center font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
         Latest work
       </h2>
     </section>
 
     <section id="contact" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">Contact</h2>
+      <h2 className="font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
+        Contact
+      </h2>
     </section>
   </Layout>
 );
