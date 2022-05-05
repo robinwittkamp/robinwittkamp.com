d37d558a41d613f6611383c8be930d3c151a6eb7
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-05 15:09:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -6,3 +7,5 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
 };
+
+module.exports = nextConfig;
