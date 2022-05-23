d9acab53f009b8f70d7d0b6d63a55407c6384b34
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-23 13:16:25 +0200

    Add preflight styling to headings

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,7 @@ const Home = () => (
     <Head />
 
     <section id="home" className="px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h1 className="font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
-        Hi, I&apos;m Robin.
-      </h1>
+      <h1 className="">Hi, I&apos;m Robin.</h1>
       <p className="mt-6 text-xl">
         I build fast, reliable websites with a simple, modern design and focus on user experience.
         The technologies I currently use the most are Next.js, React.js and WordPress.
@@ -19,21 +17,24 @@ const Home = () => (
     </section>
 
     <section id="about" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="text-center font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
-        About me
-      </h2>
+      <h2 className="text-center">About me</h2>
     </section>
 
     <section id="work" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="text-center font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
-        Latest work
-      </h2>
+      <h2 className="text-center">Latest work</h2>
     </section>
 
     <section id="contact" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-      <h2 className="font-gilroy text-5xl font-extrabold md:text-6xl lg:text-7xl xl:text-8xl">
-        Contact
-      </h2>
+      <h2 className="">Contact</h2>
+    </section>
+
+    <section id="" className="mt-48 px-4 py-24 sm:px-8 md:px-12 lg:px-24 xl:px-32">
+      <h1 className="">Heading 1</h1>
+      <h2 className="">Heading 2</h2>
+      <h3 className="">Heading 3</h3>
+      <h4 className="">Heading 4</h4>
+      <h5 className="">Heading 5</h5>
+      <h6 className="">Heading 6</h6>
     </section>
   </Layout>
 );
