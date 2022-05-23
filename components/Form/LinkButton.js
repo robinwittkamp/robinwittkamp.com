3689ec84814815568fec44b63cf6b0eeafb86b9d
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-23 15:36:44 +0200

    LinkButton: improve styling

--- components/Form/LinkButton.js
+++ components/Form/LinkButton.js
@@ -3,7 +3,7 @@
 const LinkButton = ({ children, link }) => (
   <a
     href={link}
-    className="mt-6 inline-flex items-center rounded-2xl border border-transparent bg-neutral-900 px-6 py-3 text-lg font-extrabold text-white shadow-md hover:bg-neutral-700"
+    className="mt-6 inline-flex items-center rounded-2xl border border-transparent bg-neutral-800 px-6 py-3 text-lg font-extrabold text-white shadow-lg shadow-blue-500/75 transition duration-300 hover:bg-neutral-900 hover:shadow-md hover:shadow-blue-500/75 dark:bg-white dark:text-neutral-900 dark:hover:bg-neutral-100"
   >
     {children}
   </a>
