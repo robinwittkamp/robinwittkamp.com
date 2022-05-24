649d2f2b8ee8d87426cebacdf25f859222cad6f0
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-24 21:24:23 +0200

    Layout: add margins on main-tag for header height

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 const Layout = ({ children }) => (
   <>
     <Header />
-    <main>{children}</main>
+    <main className="mt-12 md:mt-20">{children}</main>
     <Footer />
   </>
 );
