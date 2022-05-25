c2dd514c42946ca58282d113c5c045a51594b90f
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-25 13:54:17 +0200

    npm install, LinkButton: use 'classnames-template-literals'

--- components/Form/LinkButton.js
+++ components/Form/LinkButton.js
@@ -1,38 +1,38 @@
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
+import ctl from '@netlify/classnames-template-literals';
+import Link from 'next/link';
+
+const variants = {
+  primary: `
+    border-transparent
+    bg-neutral-800
+    text-white
+    shadow-lg
+    shadow-blue-500/50
+    hover:bg-neutral-900
+    hover:shadow-blue-500/50
+    dark:bg-white
+    dark:text-neutral-900
+    dark:hover:bg-neutral-200`,
+  secondary: `
+    border-blue-100
+    bg-neutral-50
+    text-neutral-700
+    shadow-lg
+    shadow-blue-500/25
+    hover:bg-neutral-100
+    hover:shadow-md
+    hover:shadow-blue-500/25
+    dark:border-neutral-700
+    dark:bg-neutral-900
+    dark:text-white
+    dark:hover:bg-neutral-800`,
+};
 
 const LinkButton = ({ children, link, variant }) => {
-  const variantClasses = {
-    primary: `
-      border-transparent
-      bg-neutral-800
-      text-white
-      shadow-lg
-      shadow-blue-500/50
-      hover:bg-neutral-900
-      hover:shadow-md
-      hover:shadow-blue-500/50
-      dark:bg-white
-      dark:text-neutral-900
-      dark:hover:bg-neutral-200
-    `,
-    secondary: `
-      border-blue-100
-      bg-neutral-50
-      text-neutral-700
-      shadow-lg
-      shadow-blue-500/25
-      hover:bg-neutral-100
-      hover:shadow-md
-      hover:shadow-blue-500/25
-      dark:border-neutral-700
-      dark:bg-neutral-900
-      dark:text-white
-      dark:hover:bg-neutral-800
-    `,
-  };
+  const variantClasses = variants[variant];
 
-  const buttonClasses = clsx(
+  const classes = ctl(
     `inline-flex
     items-center
     rounded-2xl
@@ -42,14 +42,14 @@ const LinkButton = ({ children, link, variant }) => {
     text-lg
     font-extrabold
     transition
-    duration-300`,
-    variantClasses[variant]
+    duration-300
+    ${variantClasses}`
   );
 
   return (
-    <a href={link} className={buttonClasses}>
-      {children}
-    </a>
+    <Link href={link}>
+      <a className={classes}>{children}</a>
+    </Link>
   );
 };
 
