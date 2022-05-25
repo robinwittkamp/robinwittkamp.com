fb0b5955ffd13e6173f08337fe24e8da818fc49a
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-05-25 22:32:31 +0200

    HeaderUnfold: add max-width, add sub containers, fix open mobile menu bug

--- components/Header/HeaderUnfold.js
+++ components/Header/HeaderUnfold.js
@@ -16,62 +16,71 @@ const HeaderUnfold = () => {
   // const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="firefox:bg-opacity-90 fixed top-0 min-h-[3rem] w-full border-b border-neutral-200 bg-white/90 backdrop-blur-lg transition-all duration-500 dark:border-neutral-800 dark:bg-neutral-900/90 md:h-[4.5rem] lg:h-[4.5rem] xl:h-[4.5rem]">
-      {/* Logo & Navigation container */}
-      <Popover className="flex h-full items-center justify-between px-4 sm:px-8 md:px-12 lg:px-24 xl:px-32">
-        {/* Logo container */}
-        <div className="flex justify-start lg:w-0 lg:flex-1">
-          <Logo />
-        </div>
+    <header className="firefox:bg-opacity-90 fixed top-0 flex min-h-[3rem] w-full items-center border-b border-neutral-200 bg-white/90 backdrop-blur-lg transition-all duration-500 dark:border-neutral-800 dark:bg-neutral-900/90 md:h-[4.5rem]">
+      {/* Max width */}
+      <div className="mx-auto max-w-screen-xl flex-1">
+        {/* Popover */}
+        <Popover className="">
+          {/* x-Paddings */}
+          <div className="px-4 sm:px-8">
+            {/* Logo & Navigation container */}
+            <div className="flex items-center justify-between">
+              {/* Logo container */}
+              <div className="flex justify-start">
+                <Logo />
+              </div>
 
-        {/* Mobil menu button */}
-        <div className="-mr-3 md:hidden">
-          <Popover.Button className="inline-flex items-center justify-center rounded-md p-2">
-            <span className="sr-only">Open menu</span>
-            <MenuIcon className="h-8 w-8" aria-hidden="true" />
-          </Popover.Button>
-        </div>
+              {/* Mobil menu button */}
+              <div className="-mr-3 md:hidden">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md p-2">
+                  <span className="sr-only">Open menu</span>
+                  <MenuIcon className="h-8 w-8" aria-hidden="true" />
+                </Popover.Button>
+              </div>
 
-        {/* <Popover.Overlay className="fixed inset-0 bg-white/90 dark:border-neutral-800 dark:bg-neutral-900/90" /> */}
+              {/* Desktop navigation */}
+              <nav className="hidden md:block">
+                <ul className="flex">
+                  {navigation.map((item) => (
+                    <li key={item.name} className="">
+                      <Link href={item.href}>
+                        <a className="flex h-[4.5rem] items-center px-6">
+                          <span className="text-lg">{item.name}</span>
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </div>
 
-        <Transition
-          enter="transition duration-500"
-          enterFrom="scale-95 opacity-0"
-          enterTo="scale-100 opacity-100"
-          leave="transition duration-500 ease-in"
-          leaveFrom="scale-100 opacity-100"
-          leaveTo="scale-95 opacity-0"
-        >
-          {/* <Popover.Overlay className="fixed inset-0 h-screen bg-white/90 dark:border-neutral-800 dark:bg-neutral-900/90" /> */}
-          {/* Mobil navigation */}
-          <Popover.Panel className="">
-            <nav className="my-2">
-              {navigation.map((item) => (
-                <Link key={item.name} href={item.href} passHref>
-                  <Popover.Button as="a" className="block px-4 py-3">
-                    <span className="text-lg">{item.name}</span>
-                  </Popover.Button>
-                </Link>
-              ))}
-            </nav>
-          </Popover.Panel>
-        </Transition>
+          {/* <Popover.Overlay className="fixed inset-0 bg-white/90 dark:border-neutral-800 dark:bg-neutral-900/90" /> */}
 
-        {/* Desktop navigation */}
-        <nav className="hidden md:block">
-          <ul className="flex">
-            {navigation.map((item) => (
-              <li key={item.name} className="">
-                <Link href={item.href}>
-                  <a className="block px-6 py-4">
-                    <span className="text-lg">{item.name}</span>
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </Popover>
+          <Transition
+            enter="transition duration-500"
+            enterFrom="scale-95 opacity-0"
+            enterTo="scale-100 opacity-100"
+            leave="transition duration-500 ease-in"
+            leaveFrom="scale-100 opacity-100"
+            leaveTo="scale-95 opacity-0"
+          >
+            {/* <Popover.Overlay className="fixed inset-0 h-screen bg-white/90 dark:border-neutral-800 dark:bg-neutral-900/90" /> */}
+            {/* Mobil navigation */}
+            <Popover.Panel className="">
+              <nav className="my-2">
+                {navigation.map((item) => (
+                  <Link key={item.name} href={item.href} passHref>
+                    <Popover.Button as="a" className="block px-4 py-3">
+                      <span className="text-lg">{item.name}</span>
+                    </Popover.Button>
+                  </Link>
+                ))}
+              </nav>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </div>
     </header>
   );
 };
