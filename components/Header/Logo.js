1956932d652587d86b62eebaaaa7db2123be045c
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-03 22:17:26 +0200

    Logo: add placeholder icon

--- components/Header/Logo.js
+++ components/Header/Logo.js
@@ -1,10 +1,20 @@
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFeather } from '@fortawesome/pro-solid-svg-icons';
 
 const Logo = () => (
   <Link href="/">
-    <a className="font-gilroy text-xl font-extrabold md:text-2xl">
-      <span className="sr-only">Robin Wittkamp</span>
-      <span className="">Robin Wittkamp</span>
+    <a className="group">
+      <div className="flex items-center space-x-3">
+        <FontAwesomeIcon
+          icon={faFeather}
+          className="text-xl transition duration-300 group-hover:text-blue-500 dark:group-hover:text-blue-500"
+        />
+        <div className="">
+          <span className="sr-only">Robin Wittkamp</span>
+          <span className="font-gilroy text-xl font-extrabold md:text-2xl">Robin Wittkamp</span>
+        </div>
+      </div>
     </a>
   </Link>
 );
