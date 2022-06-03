6a666a0966e74f61e2054fdcc566481b105d7bc5
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-03 11:38:55 +0200

    HeaderUnfold: remove background blur if mobile menu open

--- components/Header/HeaderUnfold.js
+++ components/Header/HeaderUnfold.js
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import Link from 'next/link';
 import { Popover } from '@headlessui/react';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -66,107 +65,112 @@ const itemsAnimations = {
 
 const HeaderUnfold = () => {
   return (
-    <header className="firefox:bg-opacity-90 fixed top-0 flex min-h-[3rem] w-full items-center border-b border-neutral-200 bg-white/90 backdrop-blur-lg transition-all duration-500 dark:border-neutral-800 dark:bg-neutral-900/90 md:h-[4.5rem]">
-      <div className="flex-1">
-        {/* Popover */}
-        <Popover>
-          {({ open }) => (
-            <>
-              {/* x-Paddings */}
-              <div className="px-4 sm:px-8">
-                {/* Max width */}
-                <div className="mx-auto max-w-screen-xl ">
-                  {/* Logo & Navigation container */}
-                  <div className="flex items-center justify-between">
-                    {/* Logo container */}
-                    <div className="flex justify-start">
-                      <Logo />
-                    </div>
+    <Popover>
+      {({ open }) => (
+        <header
+          className={`firefox:bg-opacity-95 md:h-[4.5rem]} fixed top-0 flex min-h-[3rem] w-full items-center border-b border-neutral-200 bg-white/90 backdrop-blur-lg transition-all duration-500 dark:border-neutral-800 dark:bg-neutral-900/90 ${
+            open
+              ? 'firefox:bg-opacity-100 bg-white/100 backdrop-blur-none dark:bg-neutral-900/100'
+              : ''
+          }`}
+        >
+          <div className="flex-1">
+            {/* Popover */}
 
-                    {/* Mobil menu button */}
-                    <div className="-mr-3 md:hidden">
-                      <Popover.Button className="group inline-flex items-center justify-center rounded-md px-3 py-2 focus:outline-none">
-                        <span className="sr-only">Open menu</span>
-                        {/* Animated hamburger icon */}
-                        <div
-                          className="flex h-8 w-6 flex-col items-center justify-center"
-                          aria-hidden="true"
-                        >
-                          <div
-                            className={`${hamburgerLine} ${
-                              open ? 'translate-y-[0.5rem] rotate-45' : ''
-                            }`}
-                          />
-                          <div className={`${hamburgerLine} mt-1.5 ${open ? 'opacity-0' : ''}`} />
-                          <div
-                            className={`${hamburgerLine} mt-1.5 ${
-                              open ? '-translate-y-[0.5rem] -rotate-45' : ''
-                            }`}
-                          />
-                        </div>
-                      </Popover.Button>
-                    </div>
-
-                    {/* Desktop navigation */}
-                    <nav className="-mr-6 hidden md:block">
-                      <ul className="flex">
-                        {navigation.map((item) => (
-                          <li key={item.name} className="">
-                            <Link href={item.href}>
-                              <a className="flex h-[4.5rem] items-center px-6 transition hover:text-neutral-500 dark:hover:text-neutral-300">
-                                <span className="text-lg">{item.name}</span>
-                              </a>
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </nav>
+            {/* x-Paddings */}
+            <div className="px-4 sm:px-8">
+              {/* Max width */}
+              <div className="mx-auto max-w-screen-xl ">
+                {/* Logo & Navigation container */}
+                <div className="flex items-center justify-between">
+                  {/* Logo container */}
+                  <div className="flex justify-start">
+                    <Logo />
                   </div>
+
+                  {/* Mobil menu button */}
+                  <div className="-mr-3 md:hidden">
+                    <Popover.Button className="group inline-flex items-center justify-center rounded-md px-3 py-2 focus:outline-none">
+                      <span className="sr-only">Open menu</span>
+                      {/* Animated hamburger icon */}
+                      <div
+                        className="flex h-8 w-6 flex-col items-center justify-center"
+                        aria-hidden="true"
+                      >
+                        <div
+                          className={`${hamburgerLine} ${
+                            open ? 'translate-y-[0.5rem] rotate-45' : ''
+                          }`}
+                        />
+                        <div className={`${hamburgerLine} mt-1.5 ${open ? 'opacity-0' : ''}`} />
+                        <div
+                          className={`${hamburgerLine} mt-1.5 ${
+                            open ? '-translate-y-[0.5rem] -rotate-45' : ''
+                          }`}
+                        />
+                      </div>
+                    </Popover.Button>
+                  </div>
+
+                  {/* Desktop navigation */}
+                  <nav className="-mr-6 hidden md:block">
+                    <ul className="flex">
+                      {navigation.map((item) => (
+                        <li key={item.name} className="">
+                          <Link href={item.href}>
+                            <a className="flex h-[4.5rem] items-center px-6 transition hover:text-neutral-500 dark:hover:text-neutral-300">
+                              <span className="text-lg">{item.name}</span>
+                            </a>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </nav>
                 </div>
               </div>
+            </div>
 
-              {/* Mobil navigation */}
-              <AnimatePresence>
-                {open && (
-                  <Popover.Panel
-                    static
-                    className="h-screen md:hidden"
-                    as={motion.div}
-                    key="panel"
-                    initial="closed"
-                    animate="open"
-                    exit="closed"
-                    variants={panelAnimations}
-                  >
-                    <RemoveScroll>
-                      <motion.nav key="nav" variants={navAnimations}>
-                        {navigation.map((item) => (
-                          // TODO: use next/link
-                          // <Link key={item.name} href={item.href} passHref>
-                          //   <Popover.Button as="a" className="block px-4 py-3 sm:px-8">
-                          //     <span className="text-lg">{item.name}</span>
-                          //   </Popover.Button>
-                          // </Link>
-                          <motion.div key={item.name} variants={itemsAnimations}>
-                            <Popover.Button
-                              as="a"
-                              href={item.href}
-                              className="block px-4 py-3 sm:px-8"
-                            >
-                              <span className="text-lg">{item.name}</span>
-                            </Popover.Button>
-                          </motion.div>
-                        ))}
-                      </motion.nav>
-                    </RemoveScroll>
-                  </Popover.Panel>
-                )}
-              </AnimatePresence>
-            </>
-          )}
-        </Popover>
-      </div>
-    </header>
+            {/* Mobil navigation */}
+            <AnimatePresence>
+              {open && (
+                <Popover.Panel
+                  static
+                  className="h-screen md:hidden"
+                  as={motion.div}
+                  key="panel"
+                  initial="closed"
+                  animate="open"
+                  exit="closed"
+                  variants={panelAnimations}
+                >
+                  <RemoveScroll>
+                    <motion.nav key="nav" variants={navAnimations}>
+                      {navigation.map((item) => (
+                        // TODO: use next/link
+                        // <Link key={item.name} href={item.href} passHref>
+                        //   <Popover.Button as="a" className="block px-4 py-3 sm:px-8">
+                        //     <span className="text-lg">{item.name}</span>
+                        //   </Popover.Button>
+                        // </Link>
+                        <motion.div key={item.name} variants={itemsAnimations}>
+                          <Popover.Button
+                            as="a"
+                            href={item.href}
+                            className="block px-4 py-3 sm:px-8"
+                          >
+                            <span className="text-lg">{item.name}</span>
+                          </Popover.Button>
+                        </motion.div>
+                      ))}
+                    </motion.nav>
+                  </RemoveScroll>
+                </Popover.Panel>
+              )}
+            </AnimatePresence>
+          </div>
+        </header>
+      )}
+    </Popover>
   );
 };
 
