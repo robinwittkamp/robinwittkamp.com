56ba8dbde77ce3d042e99fae63ba44276239f4f2
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-03 21:25:57 +0200

    Footer: add three column layout

--- components/Footer.js
+++ components/Footer.js
@@ -13,34 +13,47 @@ const Footer = () => (
     <div className="mx-auto max-w-screen-xl ">
       {/* Padding container */}
       <div className="px-4 sm:px-8">
-        {/* Vertical space container */}
-        <div className="space-y-3">
-          {/* Languages */}
-          <ul className="flex justify-center">
-            {languages.map((item) => (
-              <li key={item.name}>
-                <Link href="#">
-                  <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">{item.name}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        {/* 3 columns container */}
+        <div className="space-y-3 md:flex md:flex-row-reverse md:justify-between md:space-y-0">
+          {/* Top/Right container */}
+          <div className="md:flex md:flex-1 md:justify-end">
+            {/* Languages */}
+            <ul className="flex justify-center">
+              {languages.map((item) => (
+                <li key={item.name}>
+                  <Link href="#">
+                    <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">
+                      {item.name}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-          {/* Links */}
-          <ul className="flex justify-center">
-            {navigation.map((item) => (
-              <li key={item.name}>
-                <Link href={item.href}>
-                  <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">{item.name}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {/* Middle container */}
+          <div className="md:flex md:flex-1 md:justify-center">
+            {/* Links */}
+            <ul className="flex justify-center">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <Link href={item.href}>
+                    <a className="block py-2 px-2 text-gray-600 dark:text-neutral-300">
+                      {item.name}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-          {/* Copyright notice */}
-          <span className="block py-2 px-2 text-center text-gray-500 dark:text-neutral-400">
-            &copy; 2022 Robin Wittkamp
-          </span>
+          {/* Bottom/left container */}
+          <div className="md:flex md:flex-1 md:justify-start">
+            {/* Copyright notice */}
+            <span className="block py-2 px-2 text-center text-gray-500 dark:text-neutral-400">
+              &copy; 2022 Robin Wittkamp
+            </span>
+          </div>
         </div>
       </div>
     </div>
