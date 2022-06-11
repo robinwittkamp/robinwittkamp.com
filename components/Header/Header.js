2909664be0e9585a037a6aacafae835ce8e14527
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-11 18:39:16 +0200

    Header: remove transparency on firefox

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -71,8 +71,10 @@ const Header = () => {
     <Popover>
       {({ open }) => (
         <motion.header
-          className={`fixed top-0 flex min-h-[3rem] w-full min-w-[20rem] items-center border-b border-neutral-200 bg-white/90 backdrop-blur-lg dark:border-neutral-800 dark:bg-neutral-900/90 md:h-[4.5rem] ${
-            isOpen ? 'bg-white/100 backdrop-blur-none dark:bg-neutral-900/100' : ''
+          className={`fixed top-0 flex min-h-[3rem] w-full min-w-[20rem] items-center border-b border-neutral-200 bg-white backdrop-blur-lg dark:border-neutral-800 dark:bg-neutral-900/90 md:h-[4.5rem] [@supports(backdrop-filter:blur(0))]:bg-white/90 [@supports(backdrop-filter:saturate(0))]:backdrop-saturate-150 ${
+            isOpen
+              ? 'bg-white backdrop-blur-none dark:bg-neutral-900/100 [@supports(backdrop-filter:blur(0))]:bg-white/100 [@supports(backdrop-filter:blur(0))]:backdrop-saturate-0'
+              : ''
           }`}
         >
           <div className="flex-1">
