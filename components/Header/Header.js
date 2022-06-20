87986f82c4d875e3ab50290250ae956ecdce0796
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-20 20:54:06 +0200

    Header: remove 'Home' link

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,7 +6,7 @@ import { RemoveScroll } from 'react-remove-scroll';
 import Logo from './Logo';
 
 const navigation = [
-  { name: 'Home', href: '#home' },
+  // { name: 'Home', href: '#home' },
   { name: 'About', href: '#about' },
   { name: 'Work', href: '#work' },
   { name: 'Contact', href: '#contact' },
