7f0748c815cfaea5cc4f8ea285c3f301d2b34d4f
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-26 17:32:36 +0200

    Update ErrorLayout.js

--- components/Layouts/ErrorLayout.js
+++ components/Layouts/ErrorLayout.js
@@ -5,7 +5,7 @@ import Section from '../Section';
 const ErrorLayout = ({ headingText, bodyText }) => (
   <Section classes="h-screen max-h-[56rem]" first>
     <h1 className="max-w-screen-lg sm:mx-auto sm:text-center">{headingText}</h1>
-    <p className="mt-8 max-w-md text-xl text-neutral-500 dark:text-neutral-400 sm:mx-auto sm:text-center">
+    <p className="mt-8 max-w-2xl text-xl text-neutral-500 dark:text-neutral-400 sm:mx-auto sm:text-center">
       {bodyText}
     </p>
     <div className="mt-8 sm:flex sm:justify-center">
