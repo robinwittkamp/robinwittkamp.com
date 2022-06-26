475058d5c2fd8f1bda6b0835cbe1bdee0328234f
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-26 16:43:45 +0200

    Section: add classes prop, increase top padding

--- components/Section.js
+++ components/Section.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 
-const Section = ({ children, first, id }) => (
+const Section = ({ children, classes, first, id }) => (
   // Padding
-  <section id={id} className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-32' : ''}`}>
+  <section
+    id={id}
+    className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-40' : ''} ${classes}`}
+  >
     {/* Max width */}
     <div className="mx-auto max-w-screen-xl">{children}</div>
   </section>
@@ -10,11 +13,13 @@ const Section = ({ children, first, id }) => (
 
 Section.propTypes = {
   children: PropTypes.node.isRequired,
+  classes: PropTypes.string,
   first: PropTypes.bool,
   id: PropTypes.string.isRequired,
 };
 
 Section.defaultProps = {
+  classes: '',
   first: false,
 };
 
