3ba5b8379c3e66c73ef26114888c7ed89bf899a5
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-26 17:21:23 +0200

    Section: remove 'classes' prop

--- components/Section.js
+++ components/Section.js
@@ -1,25 +1,18 @@
 import PropTypes from 'prop-types';
 
-const Section = ({ children, classes, first, id }) => (
-  // Padding
-  <section
-    id={id}
-    className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-40' : ''} ${classes}`}
-  >
-    {/* Max width */}
+const Section = ({ children, first, id }) => (
+  <section id={id} className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-40' : ''}`}>
     <div className="mx-auto max-w-screen-xl">{children}</div>
   </section>
 );
 
 Section.propTypes = {
   children: PropTypes.node.isRequired,
-  classes: PropTypes.string,
   first: PropTypes.bool,
   id: PropTypes.string.isRequired,
 };
 
 Section.defaultProps = {
-  classes: '',
   first: false,
 };
 
