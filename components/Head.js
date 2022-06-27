ac2c66b115455f9da4236828585cc000c7317275
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-27 14:41:40 +0200

    Head: add favicon files

--- components/Head.js
+++ components/Head.js
@@ -3,10 +3,20 @@ import NextHead from 'next/head';
 
 const Head = ({ title, og, description }) => (
   <NextHead>
+    {/* Title */}
     <title>{title}</title>
+    {/* SEO */}
     <meta name="description" content={description} />
     <meta name="robots" content="noindex, nofollow" />
-    {/* <link rel="icon" href="/favicon.ico" /> */}
+    {/* Favicon */}
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#171717" />
+    <meta name="msapplication-TileColor" content="#171717" />
+    <meta name="theme-color" content="#ffffff" />
   </NextHead>
 );
 
