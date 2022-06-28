6f88b50330d6896de59de04a08ea0c57a24622e4
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-28 16:10:46 +0200

    ProjectCard: improve font size

--- components/Cards/ProjectCard.js
+++ components/Cards/ProjectCard.js
@@ -6,10 +6,10 @@ import LinkButton from '../Form/LinkButton';
 const ProjectCard = ({ heading, link, text }) => (
   <div className="rounded-[2rem] border border-neutral-200 bg-white px-6 py-8 shadow-lg dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-lg dark:shadow-black sm:px-8 lg:p-12">
     <div className="">
-      <span className="block font-gilroy text-3xl font-bold md:text-4xl lg:text-4xl xl:text-4xl">
+      <span className="block text-2xl font-bold sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl">
         {heading}
       </span>
-      <p className="mt-6 text-lg text-neutral-500 dark:text-neutral-400">{text}</p>
+      <p className="mt-6 text-lg text-neutral-500 dark:text-neutral-400 lg:text-xl">{text}</p>
       <div className="mt-6">
         <LinkButton link={link} variant="secondary" external>
           <span>Visit website</span>
