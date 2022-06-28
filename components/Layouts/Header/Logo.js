3c44a8bce0183c685c8d7408662e3178c351feb2
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-28 16:24:12 +0200

    Headings: test increased font-weight

--- components/Layouts/Header/Logo.js
+++ components/Layouts/Header/Logo.js
@@ -7,9 +7,9 @@ const Logo = () => (
     <a className="cursor-pointer">
       <div className="flex items-center space-x-3">
         <FontAwesomeIcon icon={faFeather} className="w-5 md:w-6" />
-        <div className="">
+        <div className="flex items-center">
           <span className="sr-only">Robin Wittkamp</span>
-          <span className="font-gilroy text-xl font-extrabold md:text-2xl">Robin Wittkamp</span>
+          <span className="font-gilroy text-xl font-black md:text-2xl">Robin Wittkamp</span>
         </div>
       </div>
     </a>
