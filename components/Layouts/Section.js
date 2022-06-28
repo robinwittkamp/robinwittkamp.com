ce923dac4e020cd28d45129e0cc7b8a4345ed5e5
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-28 15:45:29 +0200

    Section: add classes prop

--- components/Layouts/Section.js
+++ components/Layouts/Section.js
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 
-const Section = ({ children, first, id }) => (
-  <section id={id} className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-40' : ''}`}>
+const Section = ({ children, classes, first, id }) => (
+  <section
+    id={id}
+    className={`py-20 px-4 sm:py-32 sm:px-8 ${first ? 'pt-28 lg:pt-40' : ''} ${classes}`}
+  >
     <div className="mx-auto max-w-screen-xl">{children}</div>
   </section>
 );
 
 Section.propTypes = {
   children: PropTypes.node.isRequired,
+  classes: PropTypes.string,
   first: PropTypes.bool,
   id: PropTypes.string,
 };
 
 Section.defaultProps = {
+  classes: '',
   first: false,
   id: null,
 };
