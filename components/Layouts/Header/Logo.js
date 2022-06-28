6b60ea72895e80dcdd5fb32c415a11ae9c0fb97b
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-06-28 11:36:42 +0200

    Logo: remove animation (framer-motion)

--- components/Layouts/Header/Logo.js
+++ components/Layouts/Header/Logo.js
@@ -1,54 +1,19 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFeather } from '@fortawesome/free-solid-svg-icons';
-import { m, LazyMotion, domAnimation } from 'framer-motion';
-
-const iconVariants = {
-  rest: {
-    scale: 1,
-    transition: {
-      type: 'spring',
-      duration: 0.5,
-    },
-  },
-  hover: {
-    scale: 1.1,
-    transition: {
-      type: 'spring',
-      duration: 0.3,
-    },
-  },
-  tap: {
-    scale: 0.9,
-    transition: {
-      type: 'spring',
-      duration: 0.3,
-    },
-  },
-};
 
 const Logo = () => (
-  <LazyMotion features={domAnimation} strict>
-    <Link href="/" passHref>
-      <m.a
-        className="cursor-pointer"
-        initial="rest"
-        animate="rest"
-        whileHover="hover"
-        whileTap="tap"
-      >
-        <div className="flex items-center space-x-3">
-          <m.div className="w-5 md:w-6" variants={iconVariants}>
-            <FontAwesomeIcon icon={faFeather} className="text-xl md:text-2xl" />
-          </m.div>
-          <div className="">
-            <span className="sr-only">Robin Wittkamp</span>
-            <span className="font-gilroy text-xl font-extrabold md:text-2xl">Robin Wittkamp</span>
-          </div>
+  <Link href="/" passHref>
+    <a className="cursor-pointer">
+      <div className="flex items-center space-x-3">
+        <FontAwesomeIcon icon={faFeather} className="w-5 md:w-6" />
+        <div className="">
+          <span className="sr-only">Robin Wittkamp</span>
+          <span className="font-gilroy text-xl font-extrabold md:text-2xl">Robin Wittkamp</span>
         </div>
-      </m.a>
-    </Link>
-  </LazyMotion>
+      </div>
+    </a>
+  </Link>
 );
 
 export default Logo;
