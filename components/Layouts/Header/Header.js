ce1394372fd2a863a6236d74c4e205d5c62cb9c0
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-07-05 13:42:29 +0200

    Add about page

--- components/Layouts/Header/Header.js
+++ components/Layouts/Header/Header.js
@@ -8,7 +8,7 @@ import Logo from './Logo';
 
 const navigation = [
   // { name: 'Home', href: '/#home' },
-  { name: 'About', href: '/#about' },
+  { name: 'About', href: '/about' },
   { name: 'Work', href: '/#work' },
   { name: 'Contact', href: '/#contact' },
 ];
---
+++ pages/about.js
@@ -0,0 +1,89 @@
+import Head from '../components/Head';
+import PageLayout from '../components/Layouts/PageLayout';
+import Section from '../components/Layouts/Section';
+
+const About = () => (
+  <PageLayout>
+    <Head title="About - Robin Wittkamp" />
+    <Section first>
+      <h1 className="text-center">About me</h1>
+      <div className="mt-16 flex">
+        <div className="flex-1" />
+        <div className="flex-1">
+          <p className="text-xl text-neutral-500 dark:text-neutral-400">
+            Hi, I&apos;m Robin. Besides my studies I work as a{' '}
+            <strong>freelance web developer</strong> and <strong>UI/UX designer</strong>. In 2016 I
+            became interested in web technologies and started implementing my own websites in my
+            spare time. In 2018 I registered a <strong>business</strong> and since then I&apos;ve
+            been implementing websites for companies and organizations from all over Germany. Today,
+            I increasingly implement websites and web applications that I program from scratch. For
+            this I use technologies like{' '}
+            <a
+              className="text-blue-500"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React.js
+            </a>{' '}
+            and{' '}
+            <a
+              className="text-blue-500"
+              href="https://nextjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Next.js
+            </a>{' '}
+            .
+            <br />
+            <br />
+            Currently I am studying{' '}
+            <a
+              className="text-blue-500"
+              href="https://www.hshl.de/en/studying/en-study-programs/en-bachelors-programs/en-visual-computing-and-design/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visual computing and Design
+            </a>{' '}
+            at the{' '}
+            <a
+              className="text-blue-500"
+              href="https://www.hshl.de/en/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Hamm-Lippstadt University of Applied Sciences
+            </a>{' '}
+            and writing my final thesis. The thesis is about generating web content from textual
+            instructions using Natural Language Processing.
+            <br />
+            <br />I am generally very interested in science and technology topics. I am especially
+            fascinated by companies in the space exploration sector like{' '}
+            <a
+              className="text-blue-500"
+              href="https://www.spacex.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              SpaceX
+            </a>{' '}
+            and companies in the renewable energy sector like{' '}
+            <a
+              className="text-blue-500"
+              href="https://www.tesla.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tesla
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </Section>
+  </PageLayout>
+);
+
+export default About;
