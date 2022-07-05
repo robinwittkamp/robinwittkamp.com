4f0290b1a713a591200b872072dd19910c26d170
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-07-05 14:32:26 +0200

    Index: add contact section with cta

--- pages/index.js
+++ pages/index.js
@@ -172,9 +172,19 @@ const Home = () => (
       </div>
     </Section>
 
-    <Section id="contact">
-      {/* <span className="font-bold text-amber-500">Let&apos;s work together.</span> */}
-      <h2 className="">Contact</h2>
+    <Section classes="bg-neutral-50 dark:bg-neutral-800">
+      {/* <div className="rounded-[2rem] bg-neutral-50 p-16 dark:bg-neutral-900"> */}
+      <h2 className="sm:text-center">Let&apos;s work together</h2>
+      <p className="mt-8 max-w-lg text-xl text-neutral-500 dark:text-neutral-400 sm:mx-auto sm:text-center">
+        If you are interested in building a project together or if you have any questions, feel free
+        to contact me.
+      </p>
+      <div className="mt-8 sm:flex sm:justify-center">
+        <LinkButton href="#work" variant="primary">
+          Get in touch
+        </LinkButton>
+      </div>
+      {/* </div> */}
     </Section>
 
     {/* <Section id="test">
