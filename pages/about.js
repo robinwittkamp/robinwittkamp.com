80ed63b0c3b37a9f675163f011d45561e271b2a1
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-07-05 19:23:53 +0200

    About: use TextBlock and TextLink components

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Head from '../components/Head';
 import PageLayout from '../components/Layouts/PageLayout';
 import Section from '../components/Layouts/Section';
 import CtaContactSection from '../components/Sections/CtaContactSection';
+import TextBlock from '../components/Text/TextBlock';
+import TextLink from '../components/Text/TextLink';
 
 const About = () => (
   <PageLayout>
@@ -11,76 +13,52 @@ const About = () => (
       <div className="mt-8 lg:mt-16 lg:flex">
         <div className="lg:flex-1" />
         <div className="lg:flex-1">
-          <p className="text-xl text-neutral-500 dark:text-neutral-400">
-            Hi, I&apos;m Robin. Besides my studies I work as a{' '}
-            <strong>freelance web developer</strong> and <strong>UI/UX designer</strong>. In 2016 I
-            became interested in web technologies and started implementing my own websites in my
-            spare time. In 2018 I registered a <strong>business</strong> and since then I&apos;ve
-            been implementing websites for companies and organizations from all over Germany. Today,
-            I increasingly implement websites and web applications that I program from scratch. For
-            this I use technologies like{' '}
-            <a
-              className="text-blue-500"
-              href="https://reactjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React.js
-            </a>{' '}
-            and{' '}
-            <a
-              className="text-blue-500"
-              href="https://nextjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Next.js
-            </a>{' '}
-            .
-            <br />
-            <br />
-            Currently I am studying{' '}
-            <a
-              className="text-blue-500"
-              href="https://www.hshl.de/en/studying/en-study-programs/en-bachelors-programs/en-visual-computing-and-design/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visual computing and Design
-            </a>{' '}
-            at the{' '}
-            <a
-              className="text-blue-500"
-              href="https://www.hshl.de/en/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Hamm-Lippstadt University of Applied Sciences
-            </a>{' '}
-            and writing my final thesis. The thesis is about generating web content from textual
-            instructions using Natural Language Processing.
-            <br />
-            <br />I am generally very interested in science and technology topics. I am especially
-            fascinated by companies in the space exploration sector like{' '}
-            <a
-              className="text-blue-500"
-              href="https://www.spacex.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              SpaceX
-            </a>{' '}
-            and companies in the renewable energy sector like{' '}
-            <a
-              className="text-blue-500"
-              href="https://www.tesla.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Tesla
-            </a>
-            .
-          </p>
+          <div className="space-y-6">
+            <TextBlock>
+              Hi, I&apos;m Robin. Besides my studies I work as a{' '}
+              <strong>freelance web developer</strong> and <strong>UI/UX designer</strong>. In 2016
+              I became interested in web technologies and started implementing my own websites in my
+              spare time. In 2018 I registered a <strong>business</strong> and since then I&apos;ve
+              been implementing websites for companies and organizations from all over Germany.
+              Today, I increasingly implement websites and web applications that I program from
+              scratch. For this I use technologies like{' '}
+              <TextLink href="https://reactjs.org/" external>
+                React.js
+              </TextLink>{' '}
+              and{' '}
+              <TextLink href="https://nextjs.org/" external>
+                Next.js
+              </TextLink>
+              .
+            </TextBlock>
+            <TextBlock>
+              Currently I am studying{' '}
+              <TextLink
+                href="https://www.hshl.de/en/studying/en-study-programs/en-bachelors-programs/en-visual-computing-and-design/"
+                external
+              >
+                Visual computing and Design
+              </TextLink>{' '}
+              at the{' '}
+              <TextLink href="https://www.hshl.de/en/" external>
+                Hamm-Lippstadt University of Applied Sciences
+              </TextLink>{' '}
+              and writing my final thesis. The thesis is about generating web content from textual
+              instructions using Natural Language Processing.
+            </TextBlock>
+            <TextBlock>
+              I am generally very interested in science and technology topics. I am especially
+              fascinated by companies in the space exploration sector like{' '}
+              <TextLink href="https://www.spacex.com/" external>
+                SpaceX
+              </TextLink>{' '}
+              and companies in the renewable energy sector like{' '}
+              <TextLink href="https://www.tesla.com/" external>
+                Tesla
+              </TextLink>
+              .
+            </TextBlock>
+          </div>
         </div>
       </div>
     </Section>
