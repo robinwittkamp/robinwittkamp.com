f2e4a77d4c9f6f6f7552ae57bdcfde8cbe34479e
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-11-20 16:35:44 +0100

    app: add types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@ import '../styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
+import type { AppProps } from 'next/app';
 
 config.autoAddCss = false;
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default App;
