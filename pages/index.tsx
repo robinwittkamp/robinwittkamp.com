6e0725ef6be907c9ba66247099ca8953ffc25d73
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-11-27 19:39:31 +0100

    index: hero: add radial gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,7 +97,7 @@ const Home = () => (
     <Head />
 
     {/* Hero */}
-    <Section first>
+    <Section first classes="bg-gradient-radial from-neutral-800 to-transparent">
       {/* Text container */}
       <div className="space-y-8">
         <div className="flex sm:justify-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
     //   gilroy: ['Gilroy', ...defaultTheme.fontFamily.sans],
     // },
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(ellipse 80% 50% at 50% -20%, var(--tw-gradient-stops))',
+      },
       fontFamily: {
         gilroy: ['var(--font-gilroy)', ...defaultTheme.fontFamily.sans],
       },
