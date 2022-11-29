d0599a7652ec40eddf132b198c2732b913d2a02f
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-11-29 16:55:08 +0100

    index: hero: heading: improve colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,7 +112,7 @@ const Home = () => (
           variant="h1"
           classes="max-w-lg sm:mx-auto sm:max-w-xl sm:text-center md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg"
         >
-          <span className="bg-gradient-to-tr from-red-600 to-amber-500 bg-clip-text text-transparent">
+          <span className="bg-gradient-to-tr from-red-600 to-amber-400 bg-clip-text text-transparent">
             Beautiful
           </span>{' '}
           websites that improve your business
