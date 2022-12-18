0b0f3c8e5840851745b7da09acbedf0f2deb0fdd
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-12-18 20:26:00 +0100

    index: comment out unused project object

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,44 +39,44 @@ type ProjectProps = {
   description: string;
 };
 
-const projects = [
-  {
-    title: 'Robert Denier',
-    href: 'https://robertdenier.de',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-  {
-    title: 'Energiewerk Rügen',
-    href: 'https://energiewerk-ruegen.de',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-  {
-    title: 'Ferienwohnung Putbus',
-    href: 'https://ferienwohnung-putbus.de',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-  {
-    title: 'WebBuddy',
-    href: 'https://webbuddy.robinwittkamp.com',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-  {
-    title: 'Klifra',
-    href: 'https://klifra.robinwittkamp.com',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-  {
-    title: 'Bauraum MV',
-    href: 'https://bauraum-mv.de',
-    description:
-      'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-  },
-];
+// const projects = [
+//   {
+//     title: 'Robert Denier',
+//     href: 'https://robertdenier.de',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+//   {
+//     title: 'Energiewerk Rügen',
+//     href: 'https://energiewerk-ruegen.de',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+//   {
+//     title: 'Ferienwohnung Putbus',
+//     href: 'https://ferienwohnung-putbus.de',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+//   {
+//     title: 'WebBuddy',
+//     href: 'https://webbuddy.robinwittkamp.com',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+//   {
+//     title: 'Klifra',
+//     href: 'https://klifra.robinwittkamp.com',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+//   {
+//     title: 'Bauraum MV',
+//     href: 'https://bauraum-mv.de',
+//     description:
+//       'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+//   },
+// ];
 
 const clientLogos = [
   {
