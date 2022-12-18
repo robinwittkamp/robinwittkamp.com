ac6cfdf354510fb3bb96c70be034fb4f37978e94
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-12-18 18:32:15 +0100

    CtaContactSection: add translations

--- components/Sections/CtaContactSection.tsx
+++ components/Sections/CtaContactSection.tsx
@@ -1,26 +1,31 @@
+import { useTranslation } from 'next-i18next';
 import type { ReactElement } from 'react';
 
 import LinkButton from '../Buttons/LinkButton';
 import Heading from '../Text/Heading';
 import Section from './Section';
 
-const CtaContactSection = (): ReactElement => (
-  <Section classes="">
-    <div className="rounded-[2rem] bg-gradient-to-tr from-rusty-800/50 to-rusty-700/50 px-6 py-12 sm:p-16 lg:p-24">
-      <Heading variant="h2" classes="sm:text-center">
-        Let&apos;s work together
-      </Heading>
-      <p className="mt-8 max-w-lg text-xl text-rusty-400 sm:mx-auto sm:text-center lg:max-w-xl lg:text-2xl">
-        If you are interested in building a project together or if you have any questions, feel free
-        to contact me.
-      </p>
-      <div className="mt-8 sm:flex sm:justify-center">
-        <LinkButton href="/contact" variant="primary">
-          Get in touch
-        </LinkButton>
+const CtaContactSection = (): ReactElement => {
+  const { t, ready } = useTranslation('common');
+  if (!ready) return <span>loading translations...</span>;
+
+  return (
+    <Section classes="">
+      <div className="rounded-[2rem] bg-gradient-to-tr from-rusty-800/50 to-rusty-700/50 px-6 py-12 sm:p-16 lg:p-24">
+        <Heading variant="h2" classes="sm:text-center">
+          {t('ctaContactSection.heading')}
+        </Heading>
+        <p className="mt-8 max-w-lg text-xl text-rusty-400 sm:mx-auto sm:text-center lg:max-w-xl lg:text-2xl">
+          {t('ctaContactSection.subheading')}
+        </p>
+        <div className="mt-8 sm:flex sm:justify-center">
+          <LinkButton href="/contact" variant="primary">
+            {t('ctaContactSection.button')}
+          </LinkButton>
+        </div>
       </div>
-    </div>
-  </Section>
-);
+    </Section>
+  );
+};
 
 export default CtaContactSection;
