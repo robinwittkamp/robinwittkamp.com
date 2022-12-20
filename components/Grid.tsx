8eaaf2740a02b69573c58ba051cd0a75879be7f5
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-12-20 14:36:47 +0100

    Grid: increase line brightness, decrease size

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,11 @@
 import type { ReactElement } from 'react';
 
 const Grid = (): ReactElement => {
-  const rows = 64;
-  const columns = 64;
+  const rows = 48;
+  const columns = 48;
 
   return (
-    <div className="pointer-events-none absolute -top-4 left-1/2 -z-50 h-full -translate-x-1/2 bg-gradient-radial-hero from-rusty-700/25 to-transparent">
+    <div className="pointer-events-none absolute -top-4 left-1/2 -z-50 h-full -translate-x-1/2 bg-gradient-radial-hero from-rusty-600/25 to-transparent">
       {/* 3D effect */}
       {/* <div className="origin-[50%_100%] [transform:perspective(5000px)rotateX(20deg)]"> */}
       {/* Rows */}
