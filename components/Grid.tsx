653fc61036cdee8337de0410114a9a09dd030028
Robin Wittkamp <robinwittkamp94@gmail.com>
2022-12-20 11:23:05 +0100

    Grid: add gradient to lines
    - use margins instead of borders

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,7 +5,7 @@ const Grid = (): ReactElement => {
   const columns = 64;
 
   return (
-    <div className="pointer-events-none absolute -top-4 left-1/2 -z-50 h-full -translate-x-1/2">
+    <div className="pointer-events-none absolute top-0 left-1/2 -z-50 h-full -translate-x-1/2 bg-gradient-radial-hero from-rusty-700/25 to-transparent">
       {/* 3D effect */}
       {/* <div className="origin-[50%_100%] [transform:perspective(5000px)rotateX(20deg)]"> */}
       {/* Rows */}
@@ -13,7 +13,7 @@ const Grid = (): ReactElement => {
         <div key={index} className="flex">
           {/* Columns */}
           {Array.from({ length: columns }, (_, index) => (
-            <div key={index} className="h-16 w-16 flex-initial border border-rusty-800/25" />
+            <div key={index} className="m-px h-16 w-16 flex-initial bg-rusty-950" />
           ))}
         </div>
       ))}
