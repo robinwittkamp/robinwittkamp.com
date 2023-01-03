6a086ee144ff32f502b10b07e5b6b4e4386289ec
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-03 13:24:37 +0100

    Update TextLink.tsx

--- components/Text/TextLink.tsx
+++ components/Text/TextLink.tsx
@@ -7,15 +7,21 @@ type TextLinkProps = {
   href: string;
 };
 
-const TextLink = ({ children, external = false, href }: TextLinkProps): ReactElement => (
-  <Link
-    className="text-blue-500 transition hover:text-blue-600"
-    href={href}
-    rel={external ? 'noopener noreferrer' : undefined}
-    target={external ? '_blank' : undefined}
-  >
-    {children}
-  </Link>
-);
+const TextLink = ({ children, external = false, href }: TextLinkProps): ReactElement => {
+  const Tag = external ? 'a' : Link;
+
+  // Set Tag to an 'a' tag if external is true, otherwise set it to a 'Link' tag
+
+  return (
+    <Tag
+      className="text-blue-400 transition hover:text-blue-500"
+      href={href}
+      rel={external ? 'noopener noreferrer' : undefined}
+      target={external ? '_blank' : undefined}
+    >
+      {children}
+    </Tag>
+  );
+};
 
 export default TextLink;
