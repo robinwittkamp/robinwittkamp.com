957b66cefd86e537dbed20a305de48d7a57dea6b
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-11 21:22:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import Grid from '../components/Backgrounds/Grid';
+// import Grid from '../components/Backgrounds/Grid';
 import LinkButton from '../components/Buttons/LinkButton';
 import ProjectCard from '../components/Cards/ProjectCard';
 import Head from '../components/Head';
