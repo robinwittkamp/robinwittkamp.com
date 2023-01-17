ab61cb50bbd02b53ed006840251a0a8dcd7dc996
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-17 21:11:01 +0100

    document: add 'selection' color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html className="bg-rusty-950 text-white antialiased">
+    <Html className="bg-rusty-950 text-white antialiased selection:bg-orange-500/75 selection:text-white">
       <Head />
       <body>
         <Main />
