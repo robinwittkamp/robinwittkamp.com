684cb6c4c170de975e2116e443b4f06957e2cfe0
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-19 14:55:53 +0100

    Add smooth scroll (has bug)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html>
+    <Html className="scroll-smooth">
       <Head />
       <body className="bg-rusty-950 text-white antialiased selection:bg-orange-500/75 selection:text-white">
         <Main />
