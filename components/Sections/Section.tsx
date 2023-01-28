97210aa35afe68e6a985671a9dd92cf3f6dafe75
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-28 17:32:21 +0100

    Section: add prop 'maxScreenWidth'

--- components/Sections/Section.tsx
+++ components/Sections/Section.tsx
@@ -5,6 +5,14 @@ type SectionProps = {
   classes?: string;
   first?: boolean;
   id?: string | undefined;
+  maxScreenWidth?: 'sm' | 'md' | 'lg' | 'xl';
+};
+
+const maxScreenWidthVariants = {
+  sm: 'max-w-screen-sm',
+  md: 'max-w-screen-md',
+  lg: 'max-w-screen-lg',
+  xl: 'max-w-screen-xl',
 };
 
 const Section = ({
@@ -12,6 +20,7 @@ const Section = ({
   classes = '',
   first = false,
   id = undefined,
+  maxScreenWidth = 'xl',
 }: SectionProps): ReactElement => (
   <section
     id={id}
@@ -19,7 +28,7 @@ const Section = ({
       first ? 'pt-28 sm:pt-32 md:pt-36 lg:pt-40' : ''
     } ${classes}`}
   >
-    <div className="mx-auto max-w-screen-xl">{children}</div>
+    <div className={`mx-auto ${maxScreenWidthVariants[maxScreenWidth]}`}>{children}</div>
   </section>
 );
 
