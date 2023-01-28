02cb3309601d131c271fde7aa9e102167cf15dcd
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-28 15:02:30 +0100

    index: projects: add more project cards

--- locales/de/index.ts
+++ locales/de/index.ts
@@ -1,9 +1,12 @@
-import BauraumDesktop from '../../public/images/bauraum-mv_desktop.png';
+// import BauraumDesktop from '../../public/images/bauraum-mv_desktop.png';
 import EnergiewerkDesktop from '../../public/images/energiewerk-ruegen_desktop.png';
 import FewoPutbusDesktop from '../../public/images/ferienwohnung-putbus_desktop.png';
 import KlifraDesktop from '../../public/images/klifra_desktop.png';
+import OnboarderPreview from '../../public/images/onboarder_mobile.png';
+import QuizBuddyDesktop from '../../public/images/quizbuddy_desktop.png';
 import RobertDenierDesktop from '../../public/images/robertdenier_desktop.png';
-import WebbuddyDesktop from '../../public/images/webbuddy_desktop.png';
+import TextToWebDesktop from '../../public/images/text-to-web_desktop.png';
+import WebBuddyDesktop from '../../public/images/webbuddy_desktop.png';
 
 export default {
   title: 'Robin Wittkamp - Webentwickler, UI/UX Designer und freelancer',
@@ -21,7 +24,7 @@ export default {
   aboutSection: {
     heading: 'Über mich',
     paragraphOne:
-      'Vor 6 Jahren startete mein Interesse für das Designen und Umsetzen von Benutzeroberflächen. Resultierend begann ich mein Studium an der Hochschule Hamm-Lippstadt im Studiengang Computervisualisitk und Design. Während des Studiums war ich als studentische Hilfskraft in verschiedenen Web- und Design-Projekten tätig. Dazu gehörte z.B. die Frontend-Entwicklung einer ERP-Software für ein mittelständisches Unternehmen.',
+      'Vor 6 Jahren begann mein Interesse für das Designen und Entwickeln von Benutzeroberflächen. Resultierend startete ich mein Studium an der Hochschule Hamm-Lippstadt im Studiengang Computervisualisitk und Design. Während des Studiums war ich als studentische Hilfskraft in verschiedenen Web- und Design-Projekten tätig. Dazu gehörte z.B. die Frontend-Entwicklung einer ERP-Software für ein mittelständisches Unternehmen.',
     paragraphTwo:
       'Im Jahr 2018 meldete ich neben dem Studium weiterhin ein Gewerbe an. Seitdem implementiere ich als Freelancer Webseiten und Webanwendungen für Unternehmen aus Deutschland. Für die Umsetzung von Projekten verwende ich sowohl bewährte Technologien wie WordPress als auch modernere Technologien wie React.js oder Next.js. Zu meinen Aufgaben gehören weiterhin das Designen von Benutzeroberflächen als auch die Administration von Servern, Domains und Webhosting allgemein.',
     skills: {
@@ -49,7 +52,7 @@ export default {
         },
         {
           heading: 'Entwickler-Tools',
-          paragraph: 'Git, npm, Visual Studio Code, ESLint, Prettier',
+          paragraph: 'Git, npm, Webpack, Visual Studio Code, ESLint, Prettier',
         },
         {
           heading: 'Design-Tools',
@@ -59,13 +62,13 @@ export default {
     },
   },
   workSection: {
-    heading: 'Neueste Projekte',
+    heading: 'Meine Projekte',
     projects: [
       {
         id: '1',
         title: 'Energiewerk Rügen',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Für die Energiewerk Rügen eG wurde sowohl das Corporate Design als auch die Webseite umgesetzt. Die Webseite wurde mit WordPress sowie benutzerdefinierten Theme und Plugins umgesetzt. Zu den verwenden Sprachen gehören HTML, CSS, JavaScript und PHP.',
         buttonText: 'Webseite ansehen',
         href: 'https://energiewerk-ruegen.de',
         image: EnergiewerkDesktop,
@@ -74,47 +77,74 @@ export default {
         id: '2',
         title: 'Robert Denier',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Corporate Design und Webseite für den Fotograf und Künstler Robert Denier. Die Webseite verwendet ein benutzerdefiniertes Shopsystem basierend auf WooCommerce. Aufbauend auf WordPress und relevatenten Plugins wurden HTML, CSS, JavaScript und PHP verwendet.',
         buttonText: 'Webseite ansehen',
         href: 'https://robertdenier.de',
         image: RobertDenierDesktop,
       },
       {
         id: '3',
-        title: 'Ferienwohnung Putbus',
+        title: 'WebBuddy',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Eine Webseite, welche als Lerninhalt für das Modul Webtechnologien an der Hochschule Hamm-Lippstadt zum Einsatz kommt. Die Webseite wurde im Rahmen meiner Arbeit als studentische Hilfskraft umgesetzt. Die Webseite verwendet Technologien wie SCSS, JavaScript, PHP Pund MySQL.',
         buttonText: 'Webseite ansehen',
-        href: 'https://ferienwohnung-putbus.de',
-        image: FewoPutbusDesktop,
+        href: 'https://webbuddy.robinwittkamp.com',
+        image: WebBuddyDesktop,
       },
       {
         id: '4',
-        title: 'WebBuddy',
-        description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-        buttonText: 'Webseite ansehen',
-        href: 'https://webbuddy.robinwittkamp.com',
-        image: WebbuddyDesktop,
-      },
-      {
-        id: '5',
         title: 'Klifra',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Eine Webseite, welche im Rahmen des Moduls Webtechnologien an der Hochschule Hamm-Lippstadt umgesetzt wurde. Das Projekt wurde als Gruppenarbeit umgesetzt. Es wurden Technologien wie HTML, CSS, JavaScript und PHP verwendet.',
         buttonText: 'Webseite ansehen',
         href: 'https://klifra.robinwittkamp.com',
         image: KlifraDesktop,
       },
       {
-        id: '6',
-        title: 'Bauraum MV',
+        id: '5',
+        title: 'Ferienwohnung Putbus',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Ein Corporate Design einschließlich einer Webseite für Ferienwohnungen in Putbus auf Rügen. Die Webseite umfasst ein System zum Anfragen und Buchen von Wohnungen. Neben WordPress kamen hierbei unter anderem Technologien wie HTML, CSS, JavaScript und PHP zum Einsatz.',
         buttonText: 'Webseite ansehen',
-        href: 'https://bauraum-mv.de',
-        image: BauraumDesktop,
+        href: 'https://ferienwohnung-putbus.de',
+        image: FewoPutbusDesktop,
       },
+      {
+        id: '6',
+        title: 'Text to Web',
+        description:
+          'Im Rahmen meiner Bachelorarbeit wurde eine Webanwendung umgesetzt, mit welcher Nutzende aus textlichen Instruktionen resultierende Webinhalte generieren und bearbeiten können. Die Anwendung wurde mit Bilbiotheken wie React.js, Next.js, TailwindCSS und unter der Verwendung von Sprachmodellen wie GPT-3 umgesetzt.',
+        buttonText: 'Code ansehen',
+        href: 'https://github.com/robinwittkamp/text-to-web',
+        image: TextToWebDesktop,
+      },
+      {
+        id: '7',
+        title: 'Onboarder',
+        description:
+          'Meine Projektarbeit absolvierte ich bei der Onboarder Gmbh & Co. KG in Dortmund. In Zusammenarbeit mit den Entscheidungsträgern und Mitarbeitenden wurde eine Onboarding-Plattform konzipiert und entwickelt. Die resultierende Webanwendung wurde mit Technologien wie React.js und SCSS verwirklicht.',
+        buttonText: 'Zum Unternehmen',
+        href: 'https://www.onboarder.de',
+        image: OnboarderPreview,
+      },
+      {
+        id: '8',
+        title: 'QuizBuddy',
+        description:
+          'Im Zuge meiner Arbeit als studentische Hilfskraft wurde in Zusammenarbeit mit Mitarbeitenden der Hochschule Hamm-Lippstadt eine Webanwendung entwickelt. Mit der Anwendung sollen Studierende bestimmte Inhalte des Moduls Webentwicklung interaktiv erlernen und üben können. Es kamen Technologien wie React.js und CSS zum Einsatz.',
+        buttonText: '',
+        href: '',
+        image: QuizBuddyDesktop,
+      },
+      // {
+      //   id: '9',
+      //   title: 'Bauraum MV',
+      //   description:
+      //     'Ein umgesetztes Corporate Design inklusive der dazugehörigen Webseite für die Bauraum MV GmbH. Die Webseite wurde mit WordPress sowie benutzerdefinierten Theme als auch angepassten Plugins umgesetzt. Zu den verwendeten Sprachen gehören z.B. HTML, CSS, JavaScript und PHP.',
+      //   buttonText: 'Webseite ansehen',
+      //   href: 'https://bauraum-mv.de',
+      //   image: BauraumDesktop,
+      // },
     ],
   },
 };
--- locales/en/index.ts
+++ locales/en/index.ts
@@ -1,9 +1,12 @@
-import BauraumDesktop from '../../public/images/bauraum-mv_desktop.png';
+// import BauraumDesktop from '../../public/images/bauraum-mv_desktop.png';
 import EnergiewerkDesktop from '../../public/images/energiewerk-ruegen_desktop.png';
 import FewoPutbusDesktop from '../../public/images/ferienwohnung-putbus_desktop.png';
 import KlifraDesktop from '../../public/images/klifra_desktop.png';
+import OnboarderPreview from '../../public/images/onboarder_mobile.png';
+import QuizBuddyDesktop from '../../public/images/quizbuddy_desktop.png';
 import RobertDenierDesktop from '../../public/images/robertdenier_desktop.png';
-import WebbuddyDesktop from '../../public/images/webbuddy_desktop.png';
+import TextToWebDesktop from '../../public/images/text-to-web_desktop.png';
+import WebBuddyDesktop from '../../public/images/webbuddy_desktop.png';
 
 export default {
   title: 'Robin Wittkamp - Web developer, UI/UX designer and freelancer',
@@ -49,7 +52,7 @@ export default {
         },
         {
           heading: 'Development tools',
-          paragraph: 'Git, npm, Visual Studio Code, ESLint, Prettier',
+          paragraph: 'Git, npm, Webpack, Visual Studio Code, ESLint, Prettier',
         },
         {
           heading: 'Design tools',
@@ -59,13 +62,13 @@ export default {
     },
   },
   workSection: {
-    heading: 'Latest work',
+    heading: 'Featured work',
     projects: [
       {
         id: '1',
         title: 'Energiewerk Rügen',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'For Energiewerk Rügen eG both the corporate design and the website were implemented. The website was implemented with WordPress as well as custom theme and plugins. The languages used include HTML, CSS, JavaScript and PHP.',
         buttonText: 'View website',
         href: 'https://energiewerk-ruegen.de',
         image: EnergiewerkDesktop,
@@ -74,47 +77,74 @@ export default {
         id: '2',
         title: 'Robert Denier',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'Corporate design and website for photographer and artist Robert Denier. The website uses a custom store system based on WooCommerce. Built on WordPress and relevant plugins, HTML, CSS, JavaScript and PHP were used.',
         buttonText: 'View website',
         href: 'https://robertdenier.de',
         image: RobertDenierDesktop,
       },
       {
         id: '3',
-        title: 'Ferienwohnung Putbus',
+        title: 'WebBuddy',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'A website, which is used as learning content for the module Webtechnologien at the University of Applied Sciences Hamm-Lippstadt. The website was implemented as part of my work as a student assistant. The website uses technologies like SCSS, JavaScript, PHP Pand MySQL.',
         buttonText: 'View website',
-        href: 'https://ferienwohnung-putbus.de',
-        image: FewoPutbusDesktop,
+        href: 'https://webbuddy.robinwittkamp.com',
+        image: WebBuddyDesktop,
       },
       {
         id: '4',
-        title: 'WebBuddy',
-        description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
-        buttonText: 'View website',
-        href: 'https://webbuddy.robinwittkamp.com',
-        image: WebbuddyDesktop,
-      },
-      {
-        id: '5',
         title: 'Klifra',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'A website that was implemented as part of the Web Technologies module at Hamm-Lippstadt University of Applied Sciences. The project was implemented as a group work. Technologies like HTML, CSS, JavaScript and PHP were used.',
         buttonText: 'View website',
         href: 'https://klifra.robinwittkamp.com',
         image: KlifraDesktop,
       },
       {
-        id: '6',
-        title: 'Bauraum MV',
+        id: '5',
+        title: 'Ferienwohnung Putbus',
         description:
-          'Sed faucibus ligula vel quam ullamcorper varius. Mauris mauris felis, tincidunt et leo ut, mattis bibendum nulla. Nunc aliquet non ipsum sit amet ullamcorper. Cras lacus magna, aliquam id nibh quis, lacinia sollicitudin arcu.',
+          'A corporate design including a website for vacation apartments in Putbus on the island of Rügen. The website includes a system for requesting and booking apartments. In addition to WordPress, technologies such as HTML, CSS, JavaScript and PHP were used here.',
         buttonText: 'View website',
-        href: 'https://bauraum-mv.de',
-        image: BauraumDesktop,
+        href: 'https://ferienwohnung-putbus.de',
+        image: FewoPutbusDesktop,
       },
+      {
+        id: '6',
+        title: 'Text to Web',
+        description:
+          'As part of my bachelor thesis, a web application was implemented with which users can generate and edit web content resulting from textual instructions. The application was implemented with libraries like React.js, Next.js, TailwindCSS and using language models like GPT-3.',
+        buttonText: 'View code',
+        href: 'https://github.com/robinwittkamp/text-to-web',
+        image: TextToWebDesktop,
+      },
+      {
+        id: '7',
+        title: 'Onboarder',
+        description:
+          'I completed my project work at Onboarder GmbH & Co. KG in Dortmund. An onboarding platform was designed and developed in cooperation with decision-makers and employees. The resulting web application was realized using technologies such as React.js and SCSS.',
+        buttonText: 'View company website',
+        href: 'https://www.onboarder.de',
+        image: OnboarderPreview,
+      },
+      {
+        id: '8',
+        title: 'QuizBuddy',
+        description:
+          'In the course of my work as a student assistant, a web application was developed in collaboration with employees of the Hamm-Lippstadt University of Applied Sciences. With the application students should be able to learn and practice certain contents of the module web development interactively. Technologies like React.js and CSS were used.',
+        buttonText: '',
+        href: '',
+        image: QuizBuddyDesktop,
+      },
+      // {
+      //   id: '9',
+      //   title: 'Bauraum MV',
+      //   description:
+      //     '',
+      //   buttonText: 'View website',
+      //   href: 'https://bauraum-mv.de',
+      //   image: BauraumDesktop,
+      // },
     ],
   },
 };
