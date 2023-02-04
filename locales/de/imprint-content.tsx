9483f78e6a7fc5db615d4807bb431911e1f60c11
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-04 12:40:02 +0100

    Imprint: Add components with localized content

---
+++ locales/de/imprint-content.tsx
@@ -0,0 +1,87 @@
+// import Link from 'next/link';
+import type { ReactElement } from 'react';
+
+import Heading from '../../components/Text/Heading';
+
+const ImprintContent = (): ReactElement => (
+  <>
+    <Heading variant="h1">Impressum</Heading>
+    <Heading variant="h6">Angaben gemäß § 5 TMG</Heading>
+    <p>
+      Robin Wittkamp
+      <br />
+      Auf dem Rott 33
+      <br />
+      59069 Hamm
+    </p>
+    <Heading variant="h6">Kontakt</Heading>
+    <p>
+      Telefon: +49-1573-0739127
+      <br />
+      E-Mail: info@robinwittkamp.com
+    </p>
+    <Heading variant="h6">Gewerbeanmeldung</Heading>
+    <p>
+      Die Gewerbeanmeldung nach § 14 GewO oder § 55 c GewO wurde am 01.10.2018 durch die Stadt Hamm
+      erteilt.
+    </p>
+    <Heading variant="h6">EU-Streitschlichtung</Heading>
+    <p>
+      Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
+      https://ec.europa.eu/consumers/odr.
+      <br />
+      Unsere E-Mail-Adresse finden Sie oben im Impressum..
+    </p>
+    <Heading variant="h6">Verbraucherstreitbeilegung / Universalschlichtungsstelle</Heading>
+    <p>
+      Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+      Verbraucherschlichtungsstelle teilzunehmen.
+    </p>
+    <Heading variant="h4">Haftungsausschluss</Heading>
+    <Heading variant="h6">Haftung für Inhalte</Heading>
+    <p>
+      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
+      allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch
+      nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+      nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+    </p>
+    <p>
+      Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
+      allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab
+      dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+      entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+    </p>
+    <Heading variant="h6">Haftung für Links</Heading>
+    <p>
+      Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
+      Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für
+      die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
+      verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche
+      Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht
+      erkennbar.
+    </p>
+    <p>
+      Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
+      Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen
+      werden wir derartige Links umgehend entfernen.
+    </p>
+    <Heading variant="h6">Urheberrecht</Heading>
+    <p>
+      Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
+      deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+      Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+      jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den
+      privaten, nicht kommerziellen Gebrauch gestattet.
+    </p>
+    <p>
+      Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die
+      Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
+      Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+      entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte
+      umgehend entfernen.
+    </p>
+    <p>Quelle: e-recht24.de</p>
+  </>
+);
+
+export default ImprintContent;
---
+++ locales/en/imprint-content.tsx
@@ -0,0 +1,83 @@
+// import Link from 'next/link';
+import type { ReactElement } from 'react';
+
+import Heading from '../../components/Text/Heading';
+
+const ImprintContent = (): ReactElement => (
+  <>
+    <Heading variant="h1">Imprint</Heading>
+    <Heading variant="h6">According to § 5 TMG</Heading>
+    <p>
+      Robin Wittkamp
+      <br />
+      Auf dem Rott 33
+      <br />
+      59069 Hamm
+      <br />
+      Germany
+    </p>
+    <Heading variant="h6">Contact</Heading>
+    <p>
+      Phone: +49-1573-0739127
+      <br />
+      Email: info@robinwittkamp.com
+    </p>
+    <Heading variant="h6">Business registration</Heading>
+    <p>
+      The business registration according to § 14 GewO or § 55 c GewO was granted by the city of
+      Hamm on 01.10.2018.
+    </p>
+    <Heading variant="h6">EU dispute resolution</Heading>
+    <p>
+      The European Commission provides a platform for online dispute resolution (ODR):
+      https://ec.europa.eu/consumers/odr.
+      <br />
+      Our e-mail address can be found above in the site notice.
+    </p>
+    <Heading variant="h6">Consumer dispute settlement / Universal arbitration</Heading>
+    <p>
+      We are not willing or obliged to participate in dispute resolution proceedings before a
+      consumer arbitration board.
+    </p>
+    <Heading variant="h4">Disclaimer</Heading>
+    <Heading variant="h6">Liability for content</Heading>
+    <p>
+      As a service provider and according to § 7 paragraph 1 of TMG, we are responsible for own
+      contents on these pages under the general laws. According to § § 8 to 10 TMG, as a service
+      provider we are not obliged to monitor transmitted or stored information from outside or to
+      investigate circumstances that indicate illegal activity.
+    </p>
+    <p>
+      Obligations under the general laws to remove or block the use of information remain
+      unaffected. However, our liability begins only when we are made aware of any specific
+      infringement. Upon notification of such violations, we will remove the content immediately.
+    </p>
+    <Heading variant="h6">Liability for links</Heading>
+    <p>
+      Our site contains links to external websites that we not control. Therefore we cannot accept
+      any responsibility for their content. The respective provider or operator of the pages is
+      always responsible for the contents of any linked site. The linked sites were checked at the
+      time of linking for possible violations of law. At that time, no illegal content was evident.
+    </p>
+    <p>
+      Without specific evidence of a violation, constant controls of the linked pages are
+      unreasonable. Upon notification of violations, we will remove such links immediately.
+    </p>
+    <Heading variant="h6">Copyright</Heading>
+    <p>
+      Content and work done on these pages created by the site operator are subject to German
+      copyright law. Duplication, processing, distribution and any kind of exploitation outside the
+      limits of copyright require the written consent of their respective author or creator.
+      Downloads and copies of these pages are only permitted for private, non-commercial use.
+    </p>
+    <p>
+      Where content is not created by the website operator, the copyright of third parties will be
+      respected. Content from third parties will be marked as such. Should you become aware of any
+      copyright infringements on these pages, please notify us. Upon notification of violations, we
+      will remove such content immediately.
+    </p>
+    <p>Source: e-recht24.de</p>
+  </>
+);
+
+export default ImprintContent;
