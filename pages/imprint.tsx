f7b9f77582b29f11a2359aa20335336ff8eb4ad4
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-04 12:40:50 +0100

    Imprint: load content form localized components

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -4,9 +4,11 @@ import { useRouter } from 'next/router';
 import Head from '../components/Head';
 import PageLayout from '../components/Layouts/PageLayout';
 import Section from '../components/Sections/Section';
-import Heading from '../components/Text/Heading';
+import UnstyledTextWrapper from '../components/Text/UnstyledTextWrapper';
 import localDe from '../locales/de/imprint';
+import ImprintContentDe from '../locales/de/imprint-content';
 import localEn from '../locales/en/imprint';
+import ImprintContentEn from '../locales/en/imprint-content';
 
 const Imprint = () => {
   const router = useRouter();
@@ -17,15 +19,9 @@ const Imprint = () => {
     <PageLayout>
       <Head title={t.title} description={t.description} noIndex />
       <Section first maxScreenWidth="md">
-        <Heading variant="h1">{t.heading}</Heading>
-        <Heading variant="h6" classes="mt-6">
-          {t.subheading1}
-        </Heading>
-        <p className="mt-4 whitespace-pre-line">{t.paragraph1}</p>
-        <Heading variant="h6" classes="mt-6">
-          {t.subheading2}
-        </Heading>
-        <p className="mt-4 whitespace-pre-line">{t.paragraph2}</p>
+        <UnstyledTextWrapper>
+          {locale === 'en' ? <ImprintContentEn /> : <ImprintContentDe />}
+        </UnstyledTextWrapper>
       </Section>
     </PageLayout>
   );
