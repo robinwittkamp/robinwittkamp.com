8e8847cfda938df98d5d9668f757d2b3c1c85505
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-04 14:21:48 +0100

    UnstyledTextWrapper: set marker color

--- components/Text/UnstyledTextWrapper.tsx
+++ components/Text/UnstyledTextWrapper.tsx
@@ -5,7 +5,7 @@ type UnstyledTextWrapperProps = {
 };
 
 const UnstyledTextWrapper = ({ children }: UnstyledTextWrapperProps): ReactElement => (
-  <div className="prose prose-invert max-w-none prose-headings:mb-4 prose-headings:mt-6 prose-headings:text-white prose-p:text-rusty-300">
+  <div className="prose prose-invert max-w-none marker:text-rusty-300 prose-headings:mb-4 prose-headings:mt-6 prose-headings:text-white prose-p:text-rusty-300">
     {children}
   </div>
 );
