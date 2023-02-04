7107aebec2052eb59a7dc4cb2eadaea142c43dd6
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-04 13:38:44 +0100

    Imprint: Remove unused code from locales

--- locales/en/imprint.ts
+++ locales/en/imprint.ts
@@ -1,14 +1,4 @@
 export default {
   title: 'Imprint - Robin Wittkamp',
   description: '',
-  heading: 'Imprint',
-  subheading1: 'According to § 5 TMG',
-  paragraph1: `
-    Robin Wittkamp\n
-    Auf dem Rott 33\n
-    59069 Hamm\n
-    Germany`,
-  subheading2: 'Contact',
-  paragraph2: `Phone: +49-1573-0739127\n
-    Email: info@robinwittkamp.com`,
 };
