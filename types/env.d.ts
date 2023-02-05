83cb5c3bc76ae8ff5a4e6f8bbc6daff322d1a071
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-05 20:14:49 +0100

    Update env.d.ts

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     SITE_URL: string;
+    MAIL_HOST: string;
+    MAIL_PORT: string;
+    MAIL_USERNAME: string;
+    MAIL_PASSWORD: string;
   }
 }
