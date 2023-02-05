b24ea55f72e100cdf5002bc40304b751ae27c5ff
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-05 12:38:55 +0100

    Contact: improve mobile layout

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -43,25 +43,25 @@ const Contact = () => {
       <Section first maxScreenWidth="lg">
         <Heading variant="h1">{t.heading}</Heading>
         {/* Flex container */}
-        <div className="mt-16 lg:flex">
+        <div className="mt-8 lg:mt-16 lg:flex">
           {/* Left container */}
           <div className="lg:flex-1">
             {/* <p className="text-lg">{t.paragraph}</p> */}
             <Heading variant="h5" classes="">
               {t.contactInformation.email}
             </Heading>
-            <p className="mt-6 text-xl text-rusty-300">info@robinwittkamp.com</p>
+            <p className="mt-4 text-lg text-rusty-300 lg:mt-6 lg:text-xl">info@robinwittkamp.com</p>
             <Heading variant="h5" classes="mt-8">
               {t.contactInformation.social}
             </Heading>
-            <div className="mt-6">
+            <div className="mt-4 lg:mt-6">
               <SocialLinks />
             </div>
           </div>
 
           {/* Right container */}
           <div className="lg:flex-1">
-            <Heading variant="h5" classes="">
+            <Heading variant="h5" classes="mt-12 lg:mt-0">
               {t.form.heading}
             </Heading>
             {/* Form */}
@@ -75,7 +75,7 @@ const Contact = () => {
                 {/* Input */}
                 <input
                   id="name"
-                  className="block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-lg text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white"
+                  className="block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white lg:text-lg"
                   type="text"
                   placeholder={t.form.fields.name}
                   {...register('name', { required: true })}
@@ -93,7 +93,7 @@ const Contact = () => {
                 {/* Input */}
                 <input
                   id="email"
-                  className="block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-lg text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white"
+                  className="block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white lg:text-lg"
                   type="email"
                   placeholder={t.form.fields.email}
                   {...register('email', { required: true })}
@@ -111,7 +111,7 @@ const Contact = () => {
                 {/* Input */}
                 <textarea
                   id="message"
-                  className="block max-h-[32rem] min-h-[8rem] w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-lg text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white"
+                  className="block max-h-[32rem] min-h-[8rem] w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 px-4 py-3 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:ring-2 focus:ring-white lg:text-lg"
                   placeholder={t.form.fields.message}
                   {...register('message', { required: true })}
                 />
@@ -124,7 +124,7 @@ const Contact = () => {
                 {t.form.fields.submit}
               </button> */}
               <LinkButton href="/" variant="secondary">
-                <PaperPlaneIcon className="mr-4 h-4" fill="currentColor" />
+                <PaperPlaneIcon className="mr-3 h-4" fill="currentColor" />
                 {t.form.fields.submit}
               </LinkButton>
             </form>
