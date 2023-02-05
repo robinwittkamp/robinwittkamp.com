9086e4f96d88a9ee83345a10cfda49fd7758d8f1
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-05 11:39:01 +0100

    Contact: add more translations

--- locales/de/contact.ts
+++ locales/de/contact.ts
@@ -2,5 +2,19 @@ export default {
   title: 'Kontakt - Robin Wittkamp',
   description:
     'Wenn Sie ein Projekt gemeinsam umsetzen möchten oder Fragen haben, kontaktieren Sie mich gerne.',
-  heading: 'Kontakt aufnehmen',
+  heading: 'Kontakt',
+  paragraph: 'Ich freue mich auf Ihre Nachricht.',
+  contactInformation: {
+    email: 'E-Mail',
+    social: 'Social',
+  },
+  form: {
+    heading: 'Nachricht senden',
+    fields: {
+      name: 'Name',
+      email: 'E-Mail',
+      message: 'Nachricht',
+      submit: 'Senden',
+    },
+  },
 };
--- locales/en/contact.ts
+++ locales/en/contact.ts
@@ -2,5 +2,19 @@ export default {
   title: 'Contact - Robin Wittkamp',
   description:
     'If you are interested in building a project together or if you have any questions, feel free to contact me.',
-  heading: 'Get in touch',
+  heading: 'Contact',
+  paragraph: 'I look forward to your message.',
+  contactInformation: {
+    email: 'Email',
+    social: 'Social',
+  },
+  form: {
+    heading: 'Send a message',
+    fields: {
+      name: 'Name',
+      email: 'Email',
+      message: 'Message',
+      submit: 'Submit',
+    },
+  },
 };
