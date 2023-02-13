95f4055d9bf5b04395742bae43d995d02abf3c26
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-13 15:50:32 +0100

    Head: Remove unused code (NextHead)

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,4 +1,3 @@
-// import NextHead from 'next/head';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import type { ReactElement } from 'react';
@@ -50,63 +49,36 @@ const Head = ({ title, description, noIndex = false }: HeadProps): ReactElement 
   }
 
   return (
-    <>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={`${process.env.SITE_URL}${currentLangRoute}${currentPath}`}
-        noindex={noIndex}
-        openGraph={{
-          title,
-          description,
-          url: `${process.env.SITE_URL}${currentLangRoute}${currentPath}`,
-          locale: currentLang,
-          images: [
-            {
-              url: `${process.env.SITE_URL}/images/robinwittkamp_og.jpg`,
-              width: 1280,
-              height: 640,
-              alt: 'Robin Wittkamp',
-            },
-          ],
-        }}
-        languageAlternates={[
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={`${process.env.SITE_URL}${currentLangRoute}${currentPath}`}
+      noindex={noIndex}
+      openGraph={{
+        title,
+        description,
+        url: `${process.env.SITE_URL}${currentLangRoute}${currentPath}`,
+        locale: currentLang,
+        images: [
           {
-            hrefLang: currentLang,
-            href: `${process.env.SITE_URL}${currentLangRoute}${currentPath}`,
+            url: `${process.env.SITE_URL}/images/robinwittkamp_og.jpg`,
+            width: 1280,
+            height: 640,
+            alt: 'Robin Wittkamp',
           },
-          {
-            hrefLang: alternativeLang,
-            href: `${process.env.SITE_URL}${alternativeLangRoute}${currentPath}`,
-          },
-        ]}
-      />
-      {/* <NextHead>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="robots" content={`${noIndex ? 'noindex' : 'index'}, follow`} />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#141313" />
-        <meta name="msapplication-TileColor" content="#141313" />
-        <meta name="theme-color" content="#141313" />
-        <meta
-          key="theme-color-light"
-          name="theme-color"
-          content="#ffffff"
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          key="theme-color-dark"
-          name="theme-color"
-          content="#141313"
-          media="(prefers-color-scheme: dark)"
-        />
-      </NextHead> */}
-    </>
+        ],
+      }}
+      languageAlternates={[
+        {
+          hrefLang: currentLang,
+          href: `${process.env.SITE_URL}${currentLangRoute}${currentPath}`,
+        },
+        {
+          hrefLang: alternativeLang,
+          href: `${process.env.SITE_URL}${alternativeLangRoute}${currentPath}`,
+        },
+      ]}
+    />
   );
 };
 
