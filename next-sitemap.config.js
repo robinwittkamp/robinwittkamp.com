431c93cd20b32319539c1b82e3087c9ef3ff1464
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-13 15:49:06 +0100

    next-sitemap config: remove unused code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
-  // generateIndexSitemap: false,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-        // disallow: '/',
-      },
-    ],
-  },
   exclude: ['*/404', '*/500', '*/imprint', '*/privacy-policy', '*/de', '*/de/*'],
   alternateRefs: [
     {
