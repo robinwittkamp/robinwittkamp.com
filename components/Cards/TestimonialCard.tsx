1e12c04cd34c65282dc9105860612b0220f0a0df
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-03-01 14:07:30 +0100

    TestimonialCard: improve layout & styling

--- components/Cards/TestimonialCard.tsx
+++ components/Cards/TestimonialCard.tsx
@@ -17,10 +17,10 @@ const TestimonialCard = ({
   position,
   testimonial,
 }: TestimonialCardCardProps): ReactElement => (
-  <div className="max-w-md rounded-[2rem] border border-rusty-700/50 bg-gradient-to-tr from-rusty-800/50 to-rusty-700/50 p-6 shadow-lg shadow-black/30 sm:p-8 md:p-10 lg:p-10">
-    <span className=" block text-lg font-bold text-rusty-50 md:text-xl lg:text-2xl">{name}</span>
+  <div className="rounded-[2rem] border border-rusty-700/50 bg-gradient-to-tr from-rusty-800/50 to-rusty-800 p-6 shadow-lg shadow-black/30 sm:p-8">
+    <p className="block text-lg text-rusty-300">{testimonial}</p>
+    <span className="mt-6 block text-lg font-bold text-rusty-100 md:text-xl">{name}</span>
     <span className="block text-rusty-400 lg:text-lg">{position}</span>
-    <p className="mt-4 block text-lg text-rusty-300 lg:text-xl">{testimonial}</p>
   </div>
 );
 
