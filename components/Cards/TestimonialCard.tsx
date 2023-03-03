de54fc68fc88c4c0fa353498d214dbbb0bc84d3b
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-03-03 13:27:37 +0100

    TestimonialCardCard: improve styling

--- components/Cards/TestimonialCard.tsx
+++ components/Cards/TestimonialCard.tsx
@@ -17,10 +17,12 @@ const TestimonialCard = ({
   position,
   testimonial,
 }: TestimonialCardCardProps): ReactElement => (
-  <div className="rounded-[2rem] border border-rusty-700/50 bg-gradient-to-tr from-rusty-800/50 to-rusty-800 p-6 shadow-lg shadow-black/30 sm:p-8">
+  <div className="flex flex-col justify-between rounded-[2rem] border border-rusty-700/50 bg-gradient-to-tr from-rusty-800/50 to-rusty-800 p-6 shadow-lg shadow-black/30 sm:p-8">
     <p className="block text-lg text-rusty-300">{testimonial}</p>
-    <span className="mt-6 block text-lg font-bold text-rusty-100 md:text-xl">{name}</span>
-    <span className="block text-rusty-400 lg:text-lg">{position}</span>
+    <div className="mt-6">
+      <span className="block text-lg font-bold text-rusty-100 md:text-xl">{name}</span>
+      <span className="block text-rusty-400 lg:text-lg">{position}</span>
+    </div>
   </div>
 );
 
