411fafc95ee9637205e68d873a24afedb58727ca
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-03-17 21:03:14 +0100

    refactor: :recycle: Simplify Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -10,12 +10,9 @@ const Logo = (): ReactElement => (
   <Link className="cursor-pointer" href="/">
     <div className="flex items-center space-x-3">
       <RobinWittkampLogo className="h-6 md:h-7" />
-      <div className="flex items-center">
-        <span className="sr-only">Robin Wittkamp</span>
-        <span className="whitespace-nowrap font-gilroy text-xl font-black md:text-2xl">
-          Robin Wittkamp
-        </span>
-      </div>
+      <span className="whitespace-nowrap font-gilroy text-xl font-black md:text-2xl">
+        Robin Wittkamp
+      </span>
     </div>
   </Link>
 );
