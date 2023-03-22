7a4dde62544c6cde85f56370f57c55d782ad8ae6
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-03-22 11:24:44 +0100

    fix: :bug: Fix incorrect icon position on contact form input field error (Fixes #8)

--- components/Form/ContactForm.tsx
+++ components/Form/ContactForm.tsx
@@ -84,80 +84,83 @@ const ContactForm = (): ReactElement => {
 
   return (
     <form className="mt-6" onSubmit={handleSubmit(onSubmit)} method="POST">
-      {/* Field: Name */}
-      <div className="relative">
+      {/* Row (Name) */}
+      <div>
         {/* Label */}
         {/* <div className="">
           <label htmlFor="name">{t.form.fields.name}</label>
         </div> */}
-        {/* Icon */}
-        <UserLargeIcon
-          className="absolute top-1/2 h-4 -translate-y-1/2 px-4 text-rusty-200"
-          fill="currentColor"
-        />
-        {/* Input */}
-        <input
-          id="name"
-          className={`block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
-            errors.name ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
-          }`}
-          type="text"
-          placeholder={t.form.fields.name}
-          aria-invalid={errors.name ? 'true' : 'false'}
-          {...register('name', { required: t.form.messages.fieldRequired })}
-        />
+        {/* Field */}
+        <div className="relative">
+          <UserLargeIcon
+            className="absolute top-1/2 h-4 -translate-y-1/2 px-4 text-rusty-200"
+            fill="currentColor"
+          />
+          <input
+            id="name"
+            className={`block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
+              errors.name ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
+            }`}
+            type="text"
+            placeholder={t.form.fields.name}
+            aria-invalid={errors.name ? 'true' : 'false'}
+            {...register('name', { required: t.form.messages.fieldRequired })}
+          />
+        </div>
         {/* Error message */}
         <FieldMessage>{errors.name?.message}</FieldMessage>
       </div>
 
-      {/* Field: Email */}
-      <div className="relative mt-4">
+      {/* Row (Email) */}
+      <div className="mt-4">
         {/* Label */}
         {/* <div className="">
           <label htmlFor="email">{t.form.fields.email}</label>
         </div> */}
-        {/* Icon */}
-        <EnvelopeIcon
-          className="absolute top-1/2 h-4 -translate-y-1/2 px-4 text-rusty-200"
-          fill="currentColor"
-        />
-        {/* Input */}
-        <input
-          id="email"
-          className={`block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
-            errors.email ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
-          }`}
-          type="email"
-          placeholder={t.form.fields.email}
-          aria-invalid={errors.email ? 'true' : 'false'}
-          {...register('email', { required: t.form.messages.fieldRequired })}
-        />
+        {/* Field */}
+        <div className="relative">
+          <EnvelopeIcon
+            className="absolute top-1/2 h-4 -translate-y-1/2 px-4 text-rusty-200"
+            fill="currentColor"
+          />
+          <input
+            id="email"
+            className={`block w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
+              errors.email ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
+            }`}
+            type="email"
+            placeholder={t.form.fields.email}
+            aria-invalid={errors.email ? 'true' : 'false'}
+            {...register('email', { required: t.form.messages.fieldRequired })}
+          />
+        </div>
         {/* Error message */}
         <FieldMessage>{errors.email?.message}</FieldMessage>
       </div>
 
-      {/* Field: Message */}
-      <div className="relative mt-4">
+      {/* Row (Message) */}
+      <div className="mt-4">
         {/* Label */}
         {/* <div className="">
           <label htmlFor="message">{t.form.fields.message}</label>
         </div> */}
-        {/* Icon */}
-        <PenClipIcon
-          className="absolute top-[calc(3.25rem/2)] h-4 -translate-y-1/2 px-4 text-rusty-200"
-          fill="currentColor"
-        />
-        {/* Input */}
-        <textarea
-          id="message"
-          className={`block max-h-[32rem] min-h-[8rem] w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
-            errors.message ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
-          }`}
-          placeholder={t.form.fields.message}
-          rows={4}
-          aria-invalid={errors.message ? 'true' : 'false'}
-          {...register('message', { required: t.form.messages.fieldRequired })}
-        />
+        {/* Field */}
+        <div className="relative">
+          <PenClipIcon
+            className="absolute top-[calc(3.25rem/2)] h-4 -translate-y-1/2 px-4 text-rusty-200"
+            fill="currentColor"
+          />
+          <textarea
+            id="message"
+            className={`block max-h-[32rem] min-h-[8rem] w-full rounded-xl border-0 border-t border-rusty-700 bg-rusty-800 py-3 pl-12 pr-4 text-white transition placeholder:text-rusty-300 focus:border-rusty-700 focus:outline-0 focus:ring-2 focus:ring-white lg:text-lg ${
+              errors.message ? 'shadow-[0_0_0_1px_rgba(255,0,0,1)]' : ''
+            }`}
+            placeholder={t.form.fields.message}
+            rows={4}
+            aria-invalid={errors.message ? 'true' : 'false'}
+            {...register('message', { required: t.form.messages.fieldRequired })}
+          />
+        </div>
         {/* Error message */}
         <FieldMessage>{errors.message?.message}</FieldMessage>
       </div>
