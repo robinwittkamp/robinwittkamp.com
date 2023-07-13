122fb0854a6b9d14bb972a1661415fe09cbef9ec
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-07-13 11:27:47 +0200

    feat: :construction: add Carousel component (WIP)

---
+++ components/Carousel.tsx
@@ -0,0 +1,28 @@
+import { AnimatePresence, LazyMotion, m, useMotionTemplate, useMotionValue } from 'framer-motion';
+import type { ReactElement, ReactNode } from 'react';
+
+/**
+ * Dynamic imports
+ */
+const loadFramerMotionFeatures = () =>
+  import('@/lib/framer-motion/framerMotionFeatures').then((res) => res.default);
+
+/**
+ * Types
+ */
+type CarouselProps = {
+  children: ReactNode | ReactNode[];
+};
+
+/**
+ * Component
+ */
+const Carousel = ({ children }: CarouselProps): ReactElement => {
+  return (
+    <LazyMotion features={loadFramerMotionFeatures} strict>
+      <div className="">{children}</div>
+    </LazyMotion>
+  );
+};
+
+export default Carousel;
